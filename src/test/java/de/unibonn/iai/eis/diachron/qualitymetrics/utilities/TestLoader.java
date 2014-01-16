556905d2cd1b7021b7103672351ddcbbbf612399
jerdeb <jerdebattista@gmail.com>
2014-01-16 11:35:53 +0100

    Added a sample dataset to test metrics and updated testloader class

--- src/test/java/de/unibonn/iai/eis/diachron/qualitymetrics/utilities/TestLoader.java
+++ src/test/java/de/unibonn/iai/eis/diachron/qualitymetrics/utilities/TestLoader.java
@@ -4,20 +4,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.hp.hpl.jena.graph.Triple;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
 
 public class TestLoader {
 
 	protected List<Triple> streamingTriples = new ArrayList<Triple>();
 	
+	/**
+	 * Loads a sample dataset which can be used to test metrics
+	 */
 	public void loadDataSet() {
-		//load dataset
-		//fill streaming triples
+		String filename = this.getClass().getClassLoader().getResource("testdumps/160114.ttl").toExternalForm();
 		
+		Model m = ModelFactory.createDefaultModel();
+		m.read(filename, "TTL");
+		
+		StmtIterator si = m.listStatements();
+		while(si.hasNext()){
+			this.streamingTriples.add(si.next().asTriple());
+		}
 	}
 	
-	//here create a method which will fill some list with all triples which will
-	//simulate the "streaming"
+	/**
+	 * Returns a list of triples from the loaded dataset. This can be used 
+	 * to simulate the streaming of triples
+	 * @return list of Triples
+	 */
 	public List<Triple> getStreamingTriples(){
 		return this.streamingTriples;
-	}
+	}	
 }
