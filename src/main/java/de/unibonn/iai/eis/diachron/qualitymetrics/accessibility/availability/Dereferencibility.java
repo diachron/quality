ec6485ebb6bf264bed137d9fff2b50c6ee86333b
nikhilpatra <nikhilpatra@gmail.com>
2014-01-21 16:05:07 +0100

    New class Dereferencibility

---
+++ src/main/java/de/unibonn/iai/eis/diachron/qualitymetrics/accessibility/availability/Dereferencibility.java
@@ -0,0 +1,30 @@
+package de.unibonn.iai.eis.diachron.qualitymetrics.accessibility.availability;
+
+import java.net.URISyntaxException;
+
+import com.hp.hpl.jena.graph.Triple;
+
+import de.unibonn.iai.eis.diachron.qualitymetrics.QualityMetric;
+
+public class Dereferencibility implements QualityMetric{
+
+	
+	@Override
+	public void compute(Triple triple) throws URISyntaxException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double metricValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
