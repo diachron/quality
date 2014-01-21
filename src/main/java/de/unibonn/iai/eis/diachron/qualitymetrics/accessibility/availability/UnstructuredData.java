82d90f506807f5d33bd177bb2217538a50876423
nikhilpatra <nikhilpatra@gmail.com>
2014-01-21 16:07:04 +0100

    New Class Unstructureddata

---
+++ src/main/java/de/unibonn/iai/eis/diachron/qualitymetrics/accessibility/availability/UnstructuredData.java
@@ -0,0 +1,27 @@
+package de.unibonn.iai.eis.diachron.qualitymetrics.accessibility.availability;
+
+import com.hp.hpl.jena.graph.Triple;
+
+import de.unibonn.iai.eis.diachron.qualitymetrics.QualityMetric;
+
+public class UnstructuredData implements QualityMetric{
+
+	@Override
+	public void compute(Triple triple) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double metricValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
