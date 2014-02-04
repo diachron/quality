6860be04b8f87afea539e19a62e5d962c3ceb570
nikhilpatra <nikhilpatra@gmail.com>
2014-02-04 14:46:29 +0100

    Added more content types to the array list of rdftypes and made changes to the code implementing the check for broken links in a different function.

--- src/main/java/de/unibonn/iai/eis/diachron/qualitymetrics/accessibility/availability/UnstructuredData.java
+++ src/main/java/de/unibonn/iai/eis/diachron/qualitymetrics/accessibility/availability/UnstructuredData.java
@@ -3,15 +3,13 @@ package de.unibonn.iai.eis.diachron.qualitymetrics.accessibility.availability;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.UnknownHostException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.apache.jena.riot.WebContent;
-
+import com.hp.hpl.jena.graph.Node;
 import com.hp.hpl.jena.graph.Triple;
 import com.hp.hpl.jena.sparql.core.Quad;
 
@@ -22,9 +20,9 @@ public class UnstructuredData implements QualityMetric{
 
 	private double deadURI;
 	private double totalURI;
-	private double notDeadURI;
 	private double metricValue;
 	private double linkWithoutRDFSupport;
+	private HttpURLConnection urlConn;
 
 	//RDF File content types reference : http://www.w3.org/2008/01/rdf-media-types
 	//ntriples RDF file uses text/plain type address this issue when there is a normal text file
@@ -32,6 +30,13 @@ public class UnstructuredData implements QualityMetric{
 	private ArrayList<String> rdfContentTypes= new ArrayList<String>(Arrays.asList("application/rdf+xml", 
 			"text/plain",
 			"application/x-turtle",
+			"application/n-triples",
+			"text/turtle",
+			"application/n3",
+			"text/n3",
+			"text/n-quads",
+			"application/rdf+json",
+			"text/x-nquads",
 			"text/rdf+n3"));
 
 	//Constructor Initializing the parameters
@@ -39,7 +44,6 @@ public class UnstructuredData implements QualityMetric{
 
 		this.deadURI = 0;
 		this.totalURI = 0;
-		this.notDeadURI = 0;
 		this.metricValue = 0;
 		this.linkWithoutRDFSupport = 0;
 	}
@@ -47,74 +51,123 @@ public class UnstructuredData implements QualityMetric{
 
 	public void compute(Quad quad) {
 
-		//Boolean variables to keep track if the triple object have a protocol scheme "http" or "https"
-		boolean hasProtocolScheme;
-		//Check if the Object is a URI        
-		if(quad.getObject().isURI())
-		{
-
-
-			try{
-				URI uriLink = new URI(quad.getObject().toString());
-				hasProtocolScheme =uriLink.getScheme().equals("http") || uriLink.getScheme().equals("https");
-				//Check if URI has been already checked
-				if(!CommonDataStructures.uriExists(uriLink) && hasProtocolScheme)
-				{
-
-					totalURI++;
-					checkedURISet.addCheckedURISet(uriLink);
-
-					//Check response
-					HttpURLConnection connection = (HttpURLConnection)uriLink.toURL().openConnection();
-					connection.setRequestMethod("GET");
-					connection.connect();
-
+		
+			Node object = quad.getObject();
+			
+			if (this.isPossibleRDFSupportingURI(object) && (!CommonDataStructures.uriExists(object))){
+				boolean isBroken = this.isBrokenURI(object);
+				
+				//checkWeb.
+				CommonDataStructures.addToUriMap(object, isBroken);
+				if (!isBroken){
+					
 					//Getting the content type of the file in the specified URL
-					String contentType = connection.getContentType();
+					String contentType = urlConn.getContentType();
+									
+					//Check for RDF supporting objects
+					if (!rdfContentTypes.contains(contentType))
+						{
+						this.linkWithoutRDFSupport++;
+						}
+					
+				}
+			}
+Node subject = quad.getSubject();
+			
+			if (this.isPossibleRDFSupportingURI(subject) && (!CommonDataStructures.uriExists(subject))){
+				boolean isBroken = this.isBrokenURI(subject);
+				
+				//checkWeb.
+				CommonDataStructures.addToUriMap(subject, isBroken);
+				if (!isBroken){
+					
+					//Getting the content type of the file in the specified URL
+					String contentType = urlConn.getContentType();
+									
 
 					//Check for RDF supporting objects
-					if (rdfContentTypes.contains(contentType))
-						linkWithoutRDFSupport++;
-
-					//Check for dead links
-					//Receive the response code for the URI 
-					//Response code for the Links reference : http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html 
-					int responseCode = connection.getResponseCode();
-					if (responseCode>=400 && responseCode<600)
-					{
-						deadURI++;
-
-					}
-					else
-					{
-						notDeadURI++;
-
-					}
-				} 
+					if (!rdfContentTypes.contains(contentType))
+						{
+						this.linkWithoutRDFSupport++;
+						}
+					
+				}
 			}
-			catch (URISyntaxException e) {
-				// Exception in Case the string given is not a URI
-				e.printStackTrace();
-
-				//Considering unknown host as dead links
-			}catch(UnknownHostException e){
-				deadURI++;
-
-				e.printStackTrace();
-
-			} catch(java.lang.ClassCastException e)
-			{
-				e.printStackTrace();
+Node predicate = quad.getPredicate();
+			
+			if (this.isPossibleRDFSupportingURI(predicate) && (!CommonDataStructures.uriExists(predicate))){
+				boolean isBroken = this.isBrokenURI(predicate);
+				
+				//checkWeb.
+				CommonDataStructures.addToUriMap(predicate, isBroken);
+				if (!isBroken){
+					
+					//Getting the content type of the file in the specified URL
+					String contentType = urlConn.getContentType();
+					
+					//Check for RDF supporting objects
+					if (!rdfContentTypes.contains(contentType))
+						{
+						this.linkWithoutRDFSupport++;
+						}
+					
+				}
 			}
-			catch (MalformedURLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} 
-
 		}
+	
+
+	
+	private boolean isBrokenURI(Node node){
+		//TODO: log more meaningful exceptions
+		Boolean isBroken = CommonDataStructures.uriExists(node) ? CommonDataStructures.isUriBroken(node) : null;
+		if (isBroken != null)
+			return isBroken;
+		else {
+			URL extUrl;
+			try {
+				extUrl = new URL(node.getURI());
+			} catch (MalformedURLException e) {
+				return true;
+			}
+
+			
+			try {
+				urlConn = (HttpURLConnection) extUrl.openConnection();
+			} catch (IOException e) {
+				return true;
+			}
+
+			try {
+				urlConn.setRequestMethod("GET");
+			} catch (ProtocolException e) {
+				return true;
+			}
+
+			int responseCode = 0;
+
+			try {
+				urlConn.connect();
+				responseCode = urlConn.getResponseCode();
+			} catch (IOException e) {
+				return true;
+			}
+
+			if (responseCode >= 200 && responseCode < 400) {
+				return false;
+			} else {
+				//If Any other response code then it is considered dead
+				this.deadURI++;
+				return true;
+			}
+		}
+	}
+	
+	private boolean isPossibleRDFSupportingURI(Node node)
+	{
+		if ((node.isURI()) && ((node.getURI().startsWith("http")) || (node.getURI().startsWith("https")))){
+			this.totalURI++;
+			return true;
+		} else return false;
 	}
 
 
