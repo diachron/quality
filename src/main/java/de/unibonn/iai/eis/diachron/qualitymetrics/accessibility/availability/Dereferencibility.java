42a981e1e1b630f9fd071f6066b6e5b31ac8ab98
jerdeb <jerdebattista@gmail.com>
2014-02-21 23:31:12 +0100

    fixed classed to reflect LOD best practices. Please Note, test classes were not updated

--- src/main/java/de/unibonn/iai/eis/diachron/qualitymetrics/accessibility/availability/Dereferencibility.java
+++ src/main/java/de/unibonn/iai/eis/diachron/qualitymetrics/accessibility/availability/Dereferencibility.java
@@ -1,28 +1,33 @@
 package de.unibonn.iai.eis.diachron.qualitymetrics.accessibility.availability;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
-import java.net.URL;
 import java.util.List;
+
+import org.apache.log4j.Logger;
 
 import com.hp.hpl.jena.graph.Node;
 import com.hp.hpl.jena.graph.Triple;
 import com.hp.hpl.jena.sparql.core.Quad;
 
 import de.unibonn.iai.eis.diachron.qualitymetrics.QualityMetric;
+import de.unibonn.iai.eis.diachron.qualitymetrics.report.accessibility.URIProfile;
 import de.unibonn.iai.eis.diachron.qualitymetrics.utilities.CommonDataStructures;
+import de.unibonn.iai.eis.diachron.qualitymetrics.utilities.HTTPConnector;
+import de.unibonn.iai.eis.diachron.qualitymetrics.utilities.HTTPConnectorReport;
 
 
 /**
  * @author 
  *
- * description of metric
+ * This metric calucate the number of valid redirects (303) according to LOD Principles
  * 
  */
 public class Dereferencibility implements QualityMetric{
 
+	static Logger logger = Logger.getLogger(Dereferencibility.class);
+	
 	private double metricValue = 0.0;
 	private double totalURI = 0;
 	private double dereferencedURI = 0;
@@ -30,32 +35,36 @@ public class Dereferencibility implements QualityMetric{
 
 	//Compute Function 	
 	public void compute(Quad quad) {
+		logger.trace("Computing Dereferencibility metric on : " + quad.asTriple());
 		Node subject = quad.getSubject();
 
-		if (this.isAPossibleDereferencableURI(subject) && (!CommonDataStructures.uriExists(subject))){
-			boolean isBroken = this.isBrokenURI(subject);
-			CommonDataStructures.addToUriMap(subject, isBroken);
-			if (!isBroken){
-				this.dereferencedURI++;
-			}
+		if (HTTPConnector.isPossibleURL(subject) && (!CommonDataStructures.uriExists(subject.getURI()))){
+			this.dereferencabilityChecker(this.buildURIProfile(subject, null));
+		} else if (CommonDataStructures.uriExists(subject.getURI())){
+			// The uri had been checked previously
+			URIProfile profile = CommonDataStructures.getURIProfile(subject.getURI());
+			if (profile.getHttpStatusCode() == 0) this.dereferencabilityChecker(this.buildURIProfile(subject, profile));
+			else this.dereferencabilityChecker(profile);
 		}
 
-		Node predicate = quad.getPredicate();
-		if (this.isAPossibleDereferencableURI(predicate) && (!CommonDataStructures.uriExists(predicate))){
-			boolean isBroken = this.isBrokenURI(predicate);
-			CommonDataStructures.addToUriMap(predicate, isBroken);
-			if (!isBroken){
-				this.dereferencedURI++;
-			}
-		}
+//		TODO: check if predicate needs to be checked for dereferencability - 
+//		it does not make sense, since the publisher do not have any control on the schema
+//		Node predicate = quad.getPredicate();
+//		if (HTTPConnector.isPossibleURL(predicate) && (!CommonDataStructures.uriExists(predicate.getURI()))){
+//			this.dereferencabilityChecker(this.buildURIProfile(predicate, null));
+//		} else if (CommonDataStructures.uriExists(predicate.getURI())){
+//			URIProfile profile = CommonDataStructures.getURIProfile(predicate.getURI());
+//			if (profile.getHttpStatusCode() == 0) this.dereferencabilityChecker(this.buildURIProfile(predicate, profile));
+//			else this.dereferencabilityChecker(profile);
+//		}
 
 		Node object = quad.getObject();
-		if (this.isAPossibleDereferencableURI(object) && (!CommonDataStructures.uriExists(object))){
-			boolean isBroken = this.isBrokenURI(object);
-			CommonDataStructures.addToUriMap(object, isBroken);
-			if (!isBroken){
-				this.dereferencedURI++;
-			}
+		if (HTTPConnector.isPossibleURL(object) && (!CommonDataStructures.uriExists(object.getURI()))){
+			this.dereferencabilityChecker(this.buildURIProfile(object, null));
+		} else if (CommonDataStructures.uriExists(object.getURI())){
+			URIProfile profile = CommonDataStructures.getURIProfile(object.getURI());
+			if (profile.getHttpStatusCode() == 0) this.dereferencabilityChecker(this.buildURIProfile(object, profile));
+			else this.dereferencabilityChecker(profile);
 		}
 	}
 
@@ -74,53 +83,31 @@ public class Dereferencibility implements QualityMetric{
 		return null;
 	}
 
-	private boolean isBrokenURI(Node node){
-		//TODO: log more meaningful exceptions
-		Boolean isBroken = CommonDataStructures.uriExists(node) ? CommonDataStructures.isUriBroken(node) : null;
-		if (isBroken != null)
-			return isBroken;
-		else {
-			URL extUrl;
-			try {
-				extUrl = new URL(node.getURI());
-			} catch (MalformedURLException e) {
-				return true;
-			}
-
-			HttpURLConnection urlConn;
-			try {
-				urlConn = (HttpURLConnection) extUrl.openConnection();
-			} catch (IOException e) {
-				return true;
-			}
-
-			try {
-				urlConn.setRequestMethod("GET");
-			} catch (ProtocolException e) {
-				return true;
-			}
-
-			int responseCode = 0;
-
-			try {
-				urlConn.connect();
-				responseCode = urlConn.getResponseCode();
-			} catch (IOException e) {
-				return true;
-			}
-
-			if (responseCode >= 200 && responseCode < 400) {
-				return false;
-			} else {
-				return true;
-			}
+	private URIProfile buildURIProfile(Node node, URIProfile p){
+		//TODO: meaningful logging
+		URIProfile profile = (p == null) ? new URIProfile() : p;
+		try {
+			HTTPConnectorReport report = HTTPConnector.connectToURI(node, false); // We want to make sure that there is no content redirection, thus 3xx codes are reported
+			// TODO: do we require to check if the redirection actually works or gives us a 404? in that case it would be a broken dereferencable URI
+			profile.setHttpStatusCode(report.getResponseCode());
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (ProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+		
+		CommonDataStructures.addToUriMap(node.getURI(), profile);
+		return profile;
 	}
 
-	private boolean isAPossibleDereferencableURI(Node node){
-		if ((node.isURI()) && ((node.getURI().startsWith("http")) || (node.getURI().startsWith("https")))){
-			this.totalURI++;
-			return true;
-		} else return false;
+	private void dereferencabilityChecker(URIProfile profile){
+		if (profile.getHttpStatusCode() == 303) { 
+			this.dereferencedURI++;
+			profile.setValidDereferencableURI(true);
+		} 
+		if (profile.getHttpStatusCode() >= 400 || profile.getHttpStatusCode() < 600) profile.setBroken(true);
+		this.totalURI++;		
 	}
 }
--- src/main/java/de/unibonn/iai/eis/diachron/qualitymetrics/accessibility/availability/UnstructuredData.java
+++ src/main/java/de/unibonn/iai/eis/diachron/qualitymetrics/accessibility/availability/UnstructuredData.java
@@ -1,174 +1,104 @@
 package de.unibonn.iai.eis.diachron.qualitymetrics.accessibility.availability;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
+
+import org.apache.log4j.Logger;
 
 import com.hp.hpl.jena.graph.Node;
 import com.hp.hpl.jena.graph.Triple;
 import com.hp.hpl.jena.sparql.core.Quad;
 
 import de.unibonn.iai.eis.diachron.qualitymetrics.QualityMetric;
+import de.unibonn.iai.eis.diachron.qualitymetrics.report.accessibility.URIProfile;
 import de.unibonn.iai.eis.diachron.qualitymetrics.utilities.CommonDataStructures;
+import de.unibonn.iai.eis.diachron.qualitymetrics.utilities.HTTPConnector;
+import de.unibonn.iai.eis.diachron.qualitymetrics.utilities.HTTPConnectorReport;
 
 public class UnstructuredData implements QualityMetric{
 
+	static Logger logger = Logger.getLogger(UnstructuredData.class);
+
+	private double unStructuredURI;
 	private double deadURI;
 	private double totalURI;
 	private double metricValue;
-	private double linkWithoutRDFSupport;
-	private HttpURLConnection urlConn;
-
-	//RDF File content types reference : http://www.w3.org/2008/01/rdf-media-types
-	//ntriples RDF file uses text/plain type address this issue when there is a normal text file
-	//Array List containing the content types of RDF files 
-	private ArrayList<String> rdfContentTypes= new ArrayList<String>(Arrays.asList("application/rdf+xml", 
-			"text/plain",
-			"application/x-turtle",
-			"application/n-triples",
-			"text/turtle",
-			"application/n3",
-			"text/n3",
-			"text/n-quads",
-			"application/rdf+json",
-			"text/x-nquads",
-			"text/rdf+n3"));
-
-	//Constructor Initializing the parameters
-	public UnstructuredData() {
-
-		this.deadURI = 0;
-		this.totalURI = 0;
-		this.metricValue = 0;
-		this.linkWithoutRDFSupport = 0;
-	}
-
 
 	public void compute(Quad quad) {
+		Date date = new Date();
+		Timestamp t = new Timestamp(date.getTime());
+		
+		logger.trace("[Unstructured Data Metric - "+ t.toString() +"] Computing metric on : " + quad.asTriple());
+		Node subject = quad.getSubject();
+
+		if (HTTPConnector.isPossibleURL(subject) && (!CommonDataStructures.uriExists(subject.getURI()))){
+			this.unStructuredDataChecker(this.buildURIProfile(subject, null));
+		} else if (CommonDataStructures.uriExists(subject.getURI())){
+			URIProfile profile = CommonDataStructures.getURIProfile(subject.getURI());
+			if (profile.getHttpStatusCode() == 0) this.unStructuredDataChecker(this.buildURIProfile(subject, profile));
+			else this.unStructuredDataChecker(profile);
+		}
+
+//		TODO: check if predicate needs to be checked for Unstructured Data - 
+//		it does not make sense, since the publisher do not have any control on the schema
+//		Node predicate = quad.getPredicate();
+//
+//		if (HTTPConnector.isPossibleURL(predicate) && (!CommonDataStructures.uriExists(predicate.getURI()))){
+//			this.unStructuredDataChecker(this.buildURIProfile(predicate, null));
+//		} else if (CommonDataStructures.uriExists(predicate.getURI())){
+//			URIProfile profile = CommonDataStructures.getURIProfile(predicate.getURI());
+//			if (profile.getHttpStatusCode() == 0) this.unStructuredDataChecker(this.buildURIProfile(predicate, profile));
+//			else this.unStructuredDataChecker(profile);
+//		}
 
 		
-			Node object = quad.getObject();
-			
-			if (this.isPossibleRDFSupportingURI(object) && (!CommonDataStructures.uriExists(object))){
-				boolean isBroken = this.isBrokenURI(object);
-				
-				//checkWeb.
-				CommonDataStructures.addToUriMap(object, isBroken);
-				if (!isBroken){
-					
-					//Getting the content type of the file in the specified URL
-					String contentType = urlConn.getContentType();
-									
-					//Check for RDF supporting objects
-					if (!rdfContentTypes.contains(contentType))
-						{
-						this.linkWithoutRDFSupport++;
-						}
-					
-				}
-			}
-Node subject = quad.getSubject();
-			
-			if (this.isPossibleRDFSupportingURI(subject) && (!CommonDataStructures.uriExists(subject))){
-				boolean isBroken = this.isBrokenURI(subject);
-				
-				//checkWeb.
-				CommonDataStructures.addToUriMap(subject, isBroken);
-				if (!isBroken){
-					
-					//Getting the content type of the file in the specified URL
-					String contentType = urlConn.getContentType();
-									
+		Node object = quad.getObject();
 
-					//Check for RDF supporting objects
-					if (!rdfContentTypes.contains(contentType))
-						{
-						this.linkWithoutRDFSupport++;
-						}
-					
-				}
-			}
-Node predicate = quad.getPredicate();
-			
-			if (this.isPossibleRDFSupportingURI(predicate) && (!CommonDataStructures.uriExists(predicate))){
-				boolean isBroken = this.isBrokenURI(predicate);
-				
-				//checkWeb.
-				CommonDataStructures.addToUriMap(predicate, isBroken);
-				if (!isBroken){
-					
-					//Getting the content type of the file in the specified URL
-					String contentType = urlConn.getContentType();
-					
-					//Check for RDF supporting objects
-					if (!rdfContentTypes.contains(contentType))
-						{
-						this.linkWithoutRDFSupport++;
-						}
-					
+		if (HTTPConnector.isPossibleURL(object) && (!CommonDataStructures.uriExists(object.getURI()))){
+			this.unStructuredDataChecker(this.buildURIProfile(object, null));
+		} else if (CommonDataStructures.uriExists(object.getURI())){
+			URIProfile profile = CommonDataStructures.getURIProfile(object.getURI());
+			if (profile.getHttpStatusCode() == 0) this.unStructuredDataChecker(this.buildURIProfile(object, profile));
+			else this.unStructuredDataChecker(profile);
+		}
+
+	
+	}
+
+	private void unStructuredDataChecker(URIProfile profile) {
+		if (profile.isBroken()) this.deadURI++;
+		if ((profile.getStructuredContentType().size() == 0) && (profile.isBroken() == false)) this.unStructuredURI++;
+		if (profile.getHttpStatusCode() >= 400 || profile.getHttpStatusCode() < 600) profile.setBroken(true);
+		this.totalURI++;
+	}
+
+	private URIProfile buildURIProfile(Node node, URIProfile p){
+		//TODO: meaningful logging
+		URIProfile profile = (p == null) ? new URIProfile() : p;
+		if ((profile.getStructuredContentType().size() == 0) && (profile.isBroken() == false)){
+			for(String content : CommonDataStructures.ldContentTypes){
+				try {
+					HTTPConnectorReport report = HTTPConnector.connectToURI(node, content, true, true); 
+					if (((report.getResponseCode() >= 400) || (report.getResponseCode() < 600)) && report.isContentParsable())
+						profile.addToStructuredContentType(content); 
+				} catch (MalformedURLException e) {
+					e.printStackTrace();
+				} catch (ProtocolException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
 				}
 			}
 		}
-	
 
-	
-	private boolean isBrokenURI(Node node){
-		//TODO: log more meaningful exceptions
-		Boolean isBroken = CommonDataStructures.uriExists(node) ? CommonDataStructures.isUriBroken(node) : null;
-		if (isBroken != null)
-			return isBroken;
-		else {
-			URL extUrl;
-			try {
-				extUrl = new URL(node.getURI());
-			} catch (MalformedURLException e) {
-				return true;
-			}
-
-			
-			try {
-				urlConn = (HttpURLConnection) extUrl.openConnection();
-			} catch (IOException e) {
-				return true;
-			}
-
-			try {
-				urlConn.setRequestMethod("GET");
-			} catch (ProtocolException e) {
-				return true;
-			}
-
-			int responseCode = 0;
-
-			try {
-				urlConn.connect();
-				responseCode = urlConn.getResponseCode();
-			} catch (IOException e) {
-				return true;
-			}
-
-			if (responseCode >= 200 && responseCode < 400) {
-				return false;
-			} else {
-				//If Any other response code then it is considered dead
-				this.deadURI++;
-				return true;
-			}
-		}
+		CommonDataStructures.addToUriMap(node.getURI(), profile);
+		return profile;
 	}
-	
-	private boolean isPossibleRDFSupportingURI(Node node)
-	{
-		if ((node.isURI()) && ((node.getURI().startsWith("http")) || (node.getURI().startsWith("https")))){
-			this.totalURI++;
-			return true;
-		} else return false;
-	}
+
 
 
 	public String getName() {
@@ -177,8 +107,8 @@ Node predicate = quad.getPredicate();
 
 
 	public double metricValue() {
-		metricValue= (deadURI+ linkWithoutRDFSupport)/totalURI;
-		return metricValue;
+		this.metricValue= (this.deadURI + this.unStructuredURI) / this.totalURI;
+		return this.metricValue;
 	}
 
 
