f28f60260ddafa6ccbafd9920bb1edf4d03065ba
jerdeb <jerdebattista@gmail.com>
2014-02-28 17:38:02 +0100

    helper classes to get information from the DAQ and to build SPARQL queries

---
+++ src/main/java/de/unibonn/iai/eis/diachron/qualitymetrics/utilities/DAQHelper.java
@@ -0,0 +1,31 @@
+package de.unibonn.iai.eis.diachron.qualitymetrics.utilities;
+
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
+import com.hp.hpl.jena.vocabulary.RDFS;
+
+import de.unibonn.iai.eis.diachron.configuration.InternalModelConf;
+
+public class DAQHelper {
+
+	private DAQHelper(){}
+	
+	public static String getClassLabel(Resource uri){
+		StmtIterator iter = InternalModelConf.getDAQModel().listStatements(uri, RDFS.label, (RDFNode) null);
+		String label = "";
+		while (iter.hasNext()){
+			label = iter.nextStatement().getObject().toString();
+		}
+		return label;
+	}
+	
+	public static String getClassDescription(Resource uri){
+		StmtIterator iter = InternalModelConf.getDAQModel().listStatements(uri, RDFS.comment, (RDFNode) null);
+		String label = "";
+		while (iter.hasNext()){
+			label = iter.nextStatement().getObject().toString();
+		}
+		return label;
+	}
+}
