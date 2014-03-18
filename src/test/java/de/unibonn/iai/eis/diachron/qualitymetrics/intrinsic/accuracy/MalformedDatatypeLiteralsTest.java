d134630c59c998c238e32135889e80530befd458
muhammadaliqasmi <muhammadaliqasmi@gmail.com>
2014-03-18 15:37:37 +0100

    Testloader improved to loaded dataset w.r.t. given test cases

--- src/test/java/de/unibonn/iai/eis/diachron/qualitymetrics/intrinsic/accuracy/MalformedDatatypeLiteralsTest.java
+++ src/test/java/de/unibonn/iai/eis/diachron/qualitymetrics/intrinsic/accuracy/MalformedDatatypeLiteralsTest.java
@@ -14,6 +14,7 @@ import org.junit.Test;
 
 import com.hp.hpl.jena.sparql.core.Quad;
 
+import de.unibonn.iai.eis.diachron.configuration.DataSetMappingForTestCase;
 import de.unibonn.iai.eis.diachron.qualitymetrics.utilities.TestLoader;
 
 /**
@@ -33,7 +34,7 @@ public class MalformedDatatypeLiteralsTest extends Assert {
 	@Before
 	public void setUp() throws Exception {
 		BasicConfigurator.configure();
-		loader.loadDataSet();
+		loader.loadDataSet(DataSetMappingForTestCase.MalformedDatatypeLiterals);
 	}
 
 	/**
