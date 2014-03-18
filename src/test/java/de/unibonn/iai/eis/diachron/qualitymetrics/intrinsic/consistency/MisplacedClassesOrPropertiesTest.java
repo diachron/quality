ffd38c84d81d309748fb82d5eadf0e0457761215
muhammadaliqasmi <muhammadaliqasmi@gmail.com>
2014-03-18 16:14:09 +0100

    isplaced Classes or properties added

---
+++ src/test/java/de/unibonn/iai/eis/diachron/qualitymetrics/intrinsic/consistency/MisplacedClassesOrPropertiesTest.java
@@ -0,0 +1,57 @@
+package de.unibonn.iai.eis.diachron.qualitymetrics.intrinsic.consistency;
+
+import java.util.List;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.hp.hpl.jena.sparql.core.Quad;
+
+import de.unibonn.iai.eis.diachron.configuration.DataSetMappingForTestCase;
+import de.unibonn.iai.eis.diachron.io.VocabularyReader;
+import de.unibonn.iai.eis.diachron.qualitymetrics.utilities.TestLoader;
+
+/**
+ * testing...
+ * @author Muhammad Ali Qasmi
+ * @date 13th March 2014
+ */
+public class MisplacedClassesOrPropertiesTest extends Assert {
+
+	static Logger logger = Logger.getLogger(MisplacedClassesOrPropertiesTest.class);
+	
+	protected TestLoader loader = new TestLoader();
+	protected MisplacedClassesOrProperties misplacedClassesOrProperties = new MisplacedClassesOrProperties(); 
+	
+	@Before
+	public void setUp() throws Exception {
+		BasicConfigurator.configure();
+		loader.loadDataSet(DataSetMappingForTestCase.MisplacedClassesOrProperties);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		VocabularyReader.clear();
+	}
+
+	@Test
+	public final void testCompute() {
+		logger.trace("testCompute() --Started--");
+		List<Quad> streamingQuads = loader.getStreamingQuads();
+		for(Quad quad : streamingQuads){
+			misplacedClassesOrProperties.compute(quad);
+		}
+		logger.info("Total Classes :: " + misplacedClassesOrProperties.getTotalClassesCount());
+		logger.info("Undefined Classes :: " + misplacedClassesOrProperties.getMisplacedClassesCount());
+		logger.info("Total Properties :: " + misplacedClassesOrProperties.getTotalPropertiesCount());
+		logger.info("Undefined Properties :: " + misplacedClassesOrProperties.getMisplacedPropertiesCount());
+		logger.info("Metric Value ::" + misplacedClassesOrProperties.metricValue());
+		assertEquals(0.13114754, misplacedClassesOrProperties.metricValue(), 0.00001);
+		logger.trace("testCompute() --Ended--");
+	}
+
+}
