f1769cd69d1ef8bf5a4fc30a3d4bb2cea3a1b88b
jerdeb <jerdebattista@gmail.com>
2014-03-20 18:12:01 +0100

    Fixed Dereferencibility Class according to the rules defined in the paper by Yang et al. - Dereferencing Semantic Web URIs: What is 200 OK on the Semantic Web?

--- src/main/java/de/unibonn/iai/eis/diachron/qualitymetrics/accessibility/availability/Dereferencibility.java
+++ src/main/java/de/unibonn/iai/eis/diachron/qualitymetrics/accessibility/availability/Dereferencibility.java
@@ -17,6 +17,9 @@ import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.sparql.core.Quad;
 import com.hp.hpl.jena.vocabulary.RDF;
 
+import de.unibonn.iai.eis.diachron.datatypes.Response;
+import de.unibonn.iai.eis.diachron.datatypes.Status;
+import de.unibonn.iai.eis.diachron.datatypes.StatusCode;
 import de.unibonn.iai.eis.diachron.qualitymetrics.QualityMetric;
 import de.unibonn.iai.eis.diachron.qualitymetrics.report.accessibility.URIProfile;
 import de.unibonn.iai.eis.diachron.qualitymetrics.utilities.CommonDataStructures;
@@ -30,20 +33,13 @@ import de.unibonn.iai.eis.diachron.vocabularies.DAQ;
  * This metric calculates the number of valid redirects (303) or hashed links
  * according to LOD Principles
  * 
- * Based on: http://www.hyperthing.org/ 
- * and http://dl.dropboxusercontent.com/u/4138729/paper/dereference_iswc2011.pdf
+ * Based on: <a href="http://www.hyperthing.org/">Hyperthing - A linked data Validator</a>
+ * 
+ * @see <a href="http://dl.dropboxusercontent.com/u/4138729/paper/dereference_iswc2011.pdf">
+ * Dereferencing Semantic Web URIs: What is 200 OK on the Semantic Web? - Yang et al.</a>
  * 
  */
 public class Dereferencibility implements QualityMetric {
-
-	/**
-	 * HTTP URI Dereferencing Model
-	 * 
-	 * 	1) Take the URI resource and make a RDF/XML request [build the Status]
-	 *	2) Get Response
-	 *	3) Map to rules and get a new status
-	 *	4) Take the temp resource and recursively determine if the deferencing succeeded
-	 */
 
 	private final Resource METRIC_URI = DAQ.DereferencibilityMetric;
 
@@ -53,10 +49,59 @@ public class Dereferencibility implements QualityMetric {
 	private double totalURI = 0;
 	private double dereferencedURI = 0;
 
+	// Compute Function
+	public void compute(Quad quad) {
+		logger.trace("Computing Dereferencibility metric on : "+ quad.asTriple());
+		//TODO: not to check if property is rdf:type
+		//TODO: idea of new metric, is the publisher using well defined vocab, i.e check if the vocab has correct dereferencable URI
+		//TODO: check if predicate needs to be checked for dereferencability - it does not make sense, since the publisher do not have any control on the schema
 
-	public Status hyperthing(Status s) throws MalformedURLException, ProtocolException, IOException{
+		try {
+			if (!(quad.getPredicate().getURI().equals(RDF.type.getURI()))){
+				this.startDereferencingProcess(quad.getSubject().getURI());//TODO: check if subject is a possible URI
+				if (HTTPConnector.isPossibleURL(quad.getObject())) this.startDereferencingProcess(quad.getObject().getURI());
+			}
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
+
+	public double metricValue() {
+		this.metricValue = this.dereferencedURI / this.totalURI;
+		return this.metricValue;
+	}
+
+	public List<Triple> toDAQTriples() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Resource getMetricURI() {
+		return this.METRIC_URI;
+	}
+
+	/**
+	 * This is the process which analyses chains of HTTP Requests and responses.
+	 * Given a Status object (RequestURI, StatusCode, TemporaryURI), this method will rewrite
+	 * a new Status (URIr, StatusCode, URIt) derived by the defined rules in Yang et al.
+	 * 
+	 * @param Status datatype consisting of the RequestURI, StatusCode (optional), TemporaryURI (optional)
+	 * @return new Status derived from the defined rules.
+	 * 
+	 * @throws MalformedURLException
+	 * @throws ProtocolException
+	 * @throws IOException
+	 */
+	private Status dereferenceNow(Status s) throws MalformedURLException, ProtocolException, IOException{
 		Response r = new Response();
-		String uri = (s.turi == "") ? s.uri : s.turi; // "" means empty response uri
+		String uri = (s.getTuri() == "") ? s.getUri() : s.getTuri(); // "" means empty response uri
 
 		boolean isHash = false;
 		if (uri.contains("#")) {
@@ -64,89 +109,101 @@ public class Dereferencibility implements QualityMetric {
 			isHash = true;
 		}
 
-		
 		// making request
 		HTTPConnectorReport rep = HTTPConnector.connectToURI(uri, WebContent.contentTypeRDFXML, false, false);
 
 		// building response
 		r = this.buildResponse(rep,isHash);
-		
+
 		Tuple t = new Tuple();
 		t.r = r;
 		t.s = s;
 
-		if ((r.sc == StatusCode.SC200) || (r.sc == StatusCode.SC4XX) || (r.sc == StatusCode.SC5XX)) return this.derefRule(t); // we do not need to continue dereferecing
-		else return this.hyperthing(this.derefRule(t));
+		if ((r.getStatusCode() == StatusCode.SC200) || 
+				(r.getStatusCode() == StatusCode.SC4XX) || 
+				(r.getStatusCode() == StatusCode.SC5XX)) return this.getRule(t); // we do not need to continue dereferecing
+		else return this.dereferenceNow(this.getRule(t));
 	}
 
-	private Status derefRule(Tuple statXresp){
+	/**
+	 * Given a Tuple of Status and Response, we generate a new Status
+	 * as per the rules defined in the paper 
+	 * Dereferencing Semantic Web URIs: What is 200 OK on the Semantic Web?
+	 * 
+	 * @see <a href="http://dl.dropboxusercontent.com/u/4138729/paper/dereference_iswc2011.pdf">
+	 * Dereferencing Semantic Web URIs: What is 200 OK on the Semantic Web?</a>
+	 * 
+	 * @param A Tuple Status X Response
+	 * @return A new status based on Rules
+	 */
+	private Status getRule(Tuple statXresp){
 		Status s = statXresp.s;
 		Response r = statXresp.r;
 		Status newStatus = new Status();
 
-		switch (r.sc){
+		switch (r.getStatusCode()){
 		case SC200 : // rule 2 and 7
-			if (s.sc == StatusCode.EMPTY) {
+			if (s.getStatusCode() == StatusCode.EMPTY) {
 				//rule 2
-				newStatus.uri = s.uri;
-				newStatus.sc = StatusCode.SC200;
-				newStatus.turi = "";
+				newStatus.setUri(s.getUri());
+				newStatus.setStatusCode(StatusCode.SC200);
+				newStatus.setTuri("");
 			}
-			if ((s.sc == StatusCode.SC303) || (s.sc == StatusCode.UNHASH)){
+			if ((s.getStatusCode() == StatusCode.SC303) || (s.getStatusCode() == StatusCode.UNHASH)){
 				//rule 7
-				newStatus.uri = s.uri;
-				newStatus.sc = s.sc;
-				newStatus.turi = s.turi;
+				newStatus.setUri(s.getUri());
+				newStatus.setStatusCode(s.getStatusCode());
+				newStatus.setTuri(s.getTuri());
 			}
 			break;
 		case SC303 : 
-			if (s.sc == StatusCode.EMPTY) {
+			if (s.getStatusCode() == StatusCode.EMPTY) {
 				//rule 4
-				newStatus.uri = s.uri;
-				newStatus.sc = StatusCode.SC303;
-				newStatus.turi = r.uri;
+				newStatus.setUri(s.getUri());
+				newStatus.setStatusCode(StatusCode.SC303);
+				newStatus.setTuri(r.getUri());
 			}
-			if ((s.sc == StatusCode.SC303) || (s.sc == StatusCode.UNHASH)){
+			if ((s.getStatusCode() == StatusCode.SC303) || (s.getStatusCode() == StatusCode.UNHASH)){
 				//rule 8
-				newStatus.uri = s.uri;
-				newStatus.sc = s.sc;
-				newStatus.turi = r.uri;
+				newStatus.setUri(s.getUri());
+				newStatus.setStatusCode(s.getStatusCode());
+				newStatus.setTuri(r.getUri());
 			}
 			break;
 		case SC301 :
 		case SC302 :
 		case SC307 :
-			if (s.sc == StatusCode.EMPTY) {
+			if (s.getStatusCode() == StatusCode.EMPTY) {
 				//rule 3
-				newStatus.uri = s.uri;
-				newStatus.sc = StatusCode.EMPTY;
-				newStatus.turi = r.uri;
+				newStatus.setUri(s.getUri());
+				newStatus.setStatusCode(StatusCode.EMPTY);
+				newStatus.setTuri(r.getUri());
 			}
-			if ((s.sc == StatusCode.SC303) || (s.sc == StatusCode.UNHASH)){
+			if ((s.getStatusCode() == StatusCode.SC303) || (s.getStatusCode() == StatusCode.UNHASH)){
 				//rule 8
-				newStatus.uri = s.uri;
-				newStatus.sc = s.sc;
-				newStatus.turi = r.uri;
+				newStatus.setUri(s.getUri());
+				newStatus.setStatusCode(s.getStatusCode());
+				newStatus.setTuri(r.getUri());
 			}
 			break;
 		case SC4XX :
 		case SC5XX :
 			//rule 5
-			newStatus.uri = s.uri;
-			newStatus.sc = StatusCode.BAD;
-			newStatus.turi = "";
+			newStatus.setUri(s.getUri());
+			newStatus.setStatusCode(StatusCode.BAD);
+			newStatus.setTuri(r.getUri());
 			break;
 		case UNHASH :
-			if ((s.sc == StatusCode.SC303) || (s.sc == StatusCode.UNHASH)){
+			if ((s.getStatusCode() == StatusCode.SC303) || (s.getStatusCode() == StatusCode.UNHASH)){
 				//rule 8
-				newStatus.uri = s.uri;
-				newStatus.sc = s.sc;
-				newStatus.turi = r.uri;
+				newStatus.setUri(s.getUri());
+				newStatus.setStatusCode(s.getStatusCode());
+				newStatus.setTuri(r.getUri());
 			} else {
-			//rule 6
-				newStatus.uri = s.uri;
-				newStatus.sc = StatusCode.UNHASH;
-				newStatus.turi = r.uri;
+				//rule 6
+				newStatus.setUri(s.getUri());
+				newStatus.setStatusCode(StatusCode.UNHASH);
+				newStatus.setTuri(r.getUri());
 			}
 			break;
 		default : break;
@@ -155,25 +212,39 @@ public class Dereferencibility implements QualityMetric {
 		return newStatus;
 	}
 
+	/**
+	 * Builds the Response (StatusCode, URI) which is used together with the Status to derived
+	 * a new Status from the rules defined by Yang et al.
+	 * 
+	 * @param HTTPConnectorReport with all the necessary details for the URI connection
+	 * @param A boolean (isHash) which is true when the URI passed is a hased URI
+	 * @return Response object (StatusCode, URI)
+	 */
 	private Response buildResponse(HTTPConnectorReport rep, boolean isHash){
 		Response r = new Response();
 		StatusCode statusCode = (isHash) ? StatusCode.UNHASH : intToStatusCode(rep.getResponseCode());
-		r.sc = statusCode;
+		r.setStatusCode(statusCode);
 
 		switch (statusCode){
 		case SC301 : 
 		case SC302 : 
 		case SC303 :
-		case SC307 : r.uri = rep.getRedirectLocation(); break;
-		case UNHASH: r.uri = rep.getUri(); break; //this is the request URI unhashed
+		case SC307 : r.setUri(rep.getRedirectLocation()); break;
+		case UNHASH: r.setUri(rep.getUri()); break; //this is the request URI unhashed
 		case SC200 : 
 		case SC4XX :
-		case SC5XX : r.uri = ""; break;
-		default : r.uri = ""; break;
+		case SC5XX : r.setUri(""); break;
+		default : r.setUri(""); break;
 		}
 		return r;
 	}
 
+	/**
+	 * Converts an HTTP Respond Code a StatusCode enum 
+	 * 
+	 * @param HTTP Response Code (integer)
+	 * @return StatusCode object
+	 */
 	private StatusCode intToStatusCode(int i){
 		if (i == 200) return StatusCode.SC200;
 		if (i == 301) return StatusCode.SC301;
@@ -185,96 +256,32 @@ public class Dereferencibility implements QualityMetric {
 		else return StatusCode.EMPTY;
 	}
 
-
-	public void testHT(String uri) throws MalformedURLException, ProtocolException, IOException{
+	/**
+	 * Starts the dereferencing process for a given URI
+	 * 
+	 * @param URI to dereference
+	 * @throws MalformedURLException
+	 * @throws ProtocolException
+	 * @throws IOException
+	 */
+	private void startDereferencingProcess(String uri) throws MalformedURLException, ProtocolException, IOException{
 		Status s = new Status();
-		s.uri = uri;
-		s.sc = StatusCode.EMPTY;
-		s.turi = ""; // "" means empty response uri
+		s.setUri(uri);
+		s.setStatusCode(StatusCode.EMPTY);
+		s.setTuri(""); // "" means empty response uri
 
-		Status ret = this.hyperthing(s);
-		
-		//System.out.println("("+ret.uri+","+ret.sc+","+ret.turi+")");
-		if ((ret.sc == StatusCode.SC303) || (ret.sc == StatusCode.UNHASH)) this.dereferencedURI++;
-		
+		Status ret = this.dereferenceNow(s);
+
+		if ((ret.getStatusCode() == StatusCode.SC303) || (ret.getStatusCode() == StatusCode.UNHASH)) this.dereferencedURI++;
 		this.totalURI++;
 	}
-	
-
-	// Compute Function
-	public void compute(Quad quad) {
-		logger.trace("Computing Dereferencibility metric on : "+ quad.asTriple());
-		//TODO: not to check if property is rdf:type
-		//TODO: idea of new metric, is the publisher using well defined vocab, i.e check if the vocab has correct dereferencable URI
-		//TODO: check if predicate needs to be checked for dereferencability - it does not make sense, since the publisher do not have any control on the schema
-	
-		if (!(quad.getPredicate().getURI().equals(RDF.type.getURI()))){
-			try {
-				this.testHT(quad.getSubject().getURI());
-			} catch (MalformedURLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (ProtocolException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} //TODO: check if subject is a possible URI
-			if (HTTPConnector.isPossibleURL(quad.getObject()))
-				try {
-					this.testHT(quad.getObject().getURI());
-				} catch (MalformedURLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (ProtocolException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-		}
-
-	
-	}
-
-	public double metricValue() {
-		System.out.println(this.dereferencedURI);
-		System.out.println(this.totalURI);
-		this.metricValue = this.dereferencedURI / this.totalURI;
-		return this.metricValue;
-	}
-
-	public List<Triple> toDAQTriples() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 
-
-
-	public Resource getMetricURI() {
-		return this.METRIC_URI;
-	}
-
-
-
-	protected class Status{
-		String uri;
-		StatusCode sc;
-		String turi;
-	}
-
-	protected class Response{
-		StatusCode sc;
-		String uri;
-	}
-
-	protected enum StatusCode{
-		SC200,SC301,SC302,SC303,SC307,SC4XX,SC5XX,UNHASH,BAD,ANY,EMPTY;
-	}
-
+	/**
+	 * This protected class is required in order to
+	 * create Dereferening Rules.
+	 *
+	 */
 	protected class Tuple{
 		Status s;
 		Response r;
