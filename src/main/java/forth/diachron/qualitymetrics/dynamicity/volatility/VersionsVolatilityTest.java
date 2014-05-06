40087a064c6f74ee10916adb6e7dacb8b4d7fc7a
hrysakis <hrysakis@.ics.forth.gr>
2014-05-06 16:21:46 +0300

    Versions Volatility Test

---
+++ src/main/java/forth/diachron/qualitymetrics/dynamicity/volatility/VersionsVolatilityTest.java
@@ -0,0 +1,14 @@
+package forth.diachron.qualitymetrics.dynamicity.volatility;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class VersionsVolatilityTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
