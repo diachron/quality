5045bc6b6948c833ad95aea35bae2395259a68f6
muhammadaliqasmi <muhammadaliqasmi@gmail.com>
2014-06-11 16:32:33 +0200

    2 level - file cache implemented for vocabulary reader.

--- src/main/java/de/unibonn/iai/eis/diachron/qualitymetrics/utilities/VocabularyReader.java
+++ src/main/java/de/unibonn/iai/eis/diachron/qualitymetrics/utilities/VocabularyReader.java
@@ -1,5 +1,10 @@
 package de.unibonn.iai.eis.diachron.qualitymetrics.utilities;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Hashtable;
 
 import org.apache.jena.atlas.web.HttpException;
@@ -17,8 +22,69 @@ import com.hp.hpl.jena.rdf.model.ModelFactory;
  */
 public final class VocabularyReader {
 	
-	private static Logger logger = Logger.getLogger(VocabularyReader.class);
+	private static String modelsFolderPath = "src/main/resources/models";
+	
+    private static Logger logger = Logger.getLogger(VocabularyReader.class);
 	private static Hashtable<String, Model>vocabularies = new Hashtable<String, Model>();
+	
+	/**
+	 * Converts url into a format that can be used as file name.
+	 * 
+	 * @param url
+	 * @return url as fileName
+	 */
+	private static String urlAsFileName(String url) {
+	        return url.replace('/', '_').replace('.', '_').replace(':', '_').replace('#','_');
+	}
+	/**
+	 * Writes contents of model to file
+	 */
+	private static void writeModelToFile(Model model, String url){
+	        try {
+	                String tmpFullPath = VocabularyReader.modelsFolderPath + "/" + VocabularyReader.urlAsFileName(url) + ".rdf";
+	                FileOutputStream fileOut = new FileOutputStream(tmpFullPath);
+	                model.write(fileOut);
+                } catch (FileNotFoundException e) {
+                        logger.debug(e.getStackTrace());
+                        logger.debug(e.getMessage());
+                }
+    }
+	
+	
+	/**
+	 * Reads contents model from file
+	 */
+	private static Model readModelFromFile(String url) {
+	        Model tmpModel = null;
+	        try {
+	                tmpModel = ModelFactory.createDefaultModel();
+	                String tmpFullPath = VocabularyReader.modelsFolderPath + "/" + VocabularyReader.urlAsFileName(url) + ".rdf";     
+                    FileInputStream fileIn = new FileInputStream(tmpFullPath);
+                    tmpModel.read(fileIn, url);
+                    fileIn.close();
+                } catch (FileNotFoundException e) {
+                        tmpModel = null;
+                        logger.debug(e.getStackTrace());
+                        logger.error(e.getMessage());
+                } catch (IOException e) {
+                        tmpModel = null;
+                        logger.debug(e.getStackTrace());
+                        logger.error(e.getMessage());
+                        
+                }
+            return tmpModel;
+	}
+	
+	/**
+	 * Checks if content of given url exists in file cache
+	 * @param url
+	 * @return true - if file exists
+	 */
+	private static boolean hasModelInFile(String url) {
+	        String tmpFullPath = VocabularyReader.modelsFolderPath + "/" + VocabularyReader.urlAsFileName(url) + ".rdf";  
+	        File file = new File(tmpFullPath);
+	        return (file.exists()) ? true : false;
+	}
 	
 	/**
 	 * Clears all content from the vocabulary hash table
@@ -37,14 +103,29 @@ public final class VocabularyReader {
 		Model model = null;
         try {
 	        if (VocabularyReader.vocabularies.containsKey(url)){
+	            // load vocabularies from memory    
 	        	logger.debug(url + " :: vocabulary loaded from cache.");
-	        	return VocabularyReader.vocabularies.get(url);
+	        	model = VocabularyReader.vocabularies.get(url);
+	        } else if (VocabularyReader.hasModelInFile(url)){
+	            // load vocabularies from file    
+                logger.debug(url + " :: vocabulary loaded from file.");
+                model = VocabularyReader.readModelFromFile(url);
+                if (model != null) {
+                        VocabularyReader.vocabularies.put(url, model);
+                }
 	        }
 	        else {
+	            if (VocabularyReader.vocabularies.containsKey(url)){
+                    return VocabularyReader.vocabularies.get(url);        
+	            }
+	            // load vocabularies from web    
 	        	logger.debug(url + " :: vocabulary loaded from web.");
 	        	model = ModelFactory.createDefaultModel();
 	        	model.read(url);
+	        	// store model in file
 	        	VocabularyReader.vocabularies.put(url, model);
+	        	// write to file
+	        	VocabularyReader.writeModelToFile(model, url);
 	        }
         }
         catch (RiotException roitException){
