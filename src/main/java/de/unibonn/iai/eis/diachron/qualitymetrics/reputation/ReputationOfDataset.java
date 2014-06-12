702a5420a3a6715541f6cc4d456d7714680245ab
muhammadaliqasmi <muhammadaliqasmi@gmail.com>
2014-06-13 01:43:42 +0200

    reputation of dataset implemented.

--- src/main/java/de/unibonn/iai/eis/diachron/qualitymetrics/reputation/ReputationOfDataset.java
+++ src/main/java/de/unibonn/iai/eis/diachron/qualitymetrics/reputation/ReputationOfDataset.java
@@ -5,13 +5,20 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
+import org.apache.xerces.util.URI;
+import org.apache.xerces.util.URI.MalformedURIException;
 
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
 import com.hp.hpl.jena.sparql.core.Quad;
 
 import de.unibonn.iai.eis.diachron.datatypes.ProblemList;
@@ -20,9 +27,15 @@ import de.unibonn.iai.eis.diachron.qualitymetrics.AbstractQualityMetric;
 import de.unibonn.iai.eis.diachron.vocabularies.DQM;
 
 /**
- * "By assigning explicit ratings to the dataset (manual) and analyzing
- * external links or page rank (semi-automated)" - Quality Assessment for Linked Open Data: A
- * Survey, Amrapali Zaveri, et al.
+ * Detects non reputable resources by retrieving URI of resources from
+ * data sets and comparing them with URI found in reputable resources.
+ * 
+ * Note: Reputable resources are stored in ../src/main/resources/reputable
+ * as file. Sub-directories of reputable directories are not evaluated.
+ * 
+ * Metric Value Range : [0 - 1]
+ * Best Case : 0
+ * Worst Case : 1
  * 
  * @author Muhammad Ali Qasmi
  * @date 12th June 2014
@@ -48,24 +61,64 @@ public class ReputationOfDataset extends AbstractQualityMetric{
         /**
          * Models of reputable sources
          */
-        protected List<Model> reputableSourcesList = new ArrayList<Model>();
+        protected Set<String> reputableSourcesURISet = new HashSet<String>();
         /**
-         * loads reputable sources models
+         * total number of resources 
+         */
+        protected long totalResources = 0;
+        /**
+         * total number of not reputable resources
+         */
+        protected long totalNotReputableResources = 0;
+        /**
+         * loads reputable sources models. Stores URI and path of each 
+         * subject, predicate, object in HashSet.
          */
         protected void loadReputableSources() {
                 try {
                 File[] files = new File(reputableSourceDirectory).listFiles();
                 for (File file : files) {
                         if (file.isFile()) {
-                                System.out.println("loading : " + file.getName());
+                                logger.debug("loading reputable resource : " + file.getName());
                                 Model model = ModelFactory.createDefaultModel();
                                 FileInputStream fileInputStream = new FileInputStream(file);
                                 model.read(fileInputStream, null);
                                 fileInputStream.close();
-                                reputableSourcesList.add(model);
+                                StmtIterator iter = model.listStatements();
+                                while (iter.hasNext()){
+                                        Statement statement = iter.nextStatement();
+                                        RDFNode subject = statement.getSubject();
+                                        RDFNode predicate = statement.getPredicate();
+                                        RDFNode object = statement.getObject();
+                                        if (subject != null){
+                                            if (subject.isURIResource()){
+                                                URI uri = new URI(subject.asNode().getURI());
+                                                if (uri != null){
+                                                        this.reputableSourcesURISet.add(uri.getHost() + "/" + uri.getPath(false,false));
+                                                }
+                                            }
+                                        }
+                                        
+                                        if (predicate != null){
+                                            if (predicate.isURIResource()){
+                                                    URI uri = new URI(predicate.asNode().getURI());
+                                                    if (uri != null){
+                                                            this.reputableSourcesURISet.add(uri.getHost() + "/" + uri.getPath(false,false));
+                                                    }
+                                            }
+                                        }
+                                        
+                                        if (object != null){
+                                            if (object.isURIResource()){
+                                                    URI uri = new URI(object.asNode().getURI());
+                                                    if (uri != null){
+                                                            this.reputableSourcesURISet.add(uri.getHost() + "/" + uri.getPath(false,false));
+                                                    }
+                                            }
+                                        }
+                                }
                         }
                     }
-                System.out.println(reputableSourcesList.size());
                 } catch (FileNotFoundException e) {
                         logger.debug(e.getStackTrace());
                         logger.error(e.getMessage());
@@ -75,15 +128,69 @@ public class ReputationOfDataset extends AbstractQualityMetric{
                 }
         }
         
+        /**
+         * Retrieves URI (with path) of each subject, predicate and object.
+         * Checks is those URI (with path) exists in the HashSet of reputable resources
+         */
         @Override
         public void compute(Quad quad) {
-                // TODO Auto-generated method stub
+                try {
+                    
+                    boolean isProblematicQuad = false;
+                    if (quad.getSubject().isURI()){
+                            this.totalResources++;    
+                            URI uri = new URI(quad.getSubject().getURI());
+                            if (!this.reputableSourcesURISet.contains(uri.getHost() + "/" + uri.getPath(false,false))){
+                                    this.totalNotReputableResources++;
+                                    isProblematicQuad = true;
+                            }
+                        }
+                    
+                    if (quad.getPredicate().isURI()){
+                        this.totalResources++;    
+                        URI uri = new URI(quad.getPredicate().getURI());
+                        if (!this.reputableSourcesURISet.contains(uri.getHost() + "/" + uri.getPath(false,false))){
+                                this.totalNotReputableResources++;
+                                isProblematicQuad = true;
+                        }
+                    }
+                    
+                    if (quad.getObject().isURI()){
+                            this.totalResources++;    
+                            URI uri = new URI(quad.getObject().getURI());
+                            if (!this.reputableSourcesURISet.contains(uri.getHost() + "/" + uri.getPath(false,false))){
+                                    this.totalNotReputableResources++;
+                                    isProblematicQuad = true;
+                            }
+                        }
+                    
+                    if (isProblematicQuad){
+                            this.problemList.add(quad);
+                    }
+                    
+                } catch (MalformedURIException e) {
+                        logger.debug(e.getStackTrace());
+                        logger.error(e.getMessage());
+                } catch (Exception e){
+                        logger.debug(e.getStackTrace());
+                        logger.error(e.getMessage());
+                }
         }
-
+        
+        /**
+         * metric value = total number of NOT reputable resources divided by total number of reputable resources
+         * 
+         * @return  (total number of NOT reputable resources / total number of reputable resources)
+         */
         @Override
         public double metricValue() {
-                // TODO Auto-generated method stub
-                return 0;
+                logger.debug("Total not Reputable Resources : " + this.totalNotReputableResources);
+                logger.debug("Total Resources : " + this.totalResources);
+                if (this.totalResources <= 0) {
+                        logger.warn("Number of total resource are ZERO");
+                        return 0;
+                }
+                return ((double) this.totalNotReputableResources / (double) this.totalResources);
         }
 
         /*
--- src/test/java/de/unibonn/iai/eis/diachron/qualitymetrics/reputation/ReputationOfDatasetTest.java
+++ src/test/java/de/unibonn/iai/eis/diachron/qualitymetrics/reputation/ReputationOfDatasetTest.java
@@ -2,14 +2,17 @@ package de.unibonn.iai.eis.diachron.qualitymetrics.reputation;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.hp.hpl.jena.sparql.core.Quad;
+
 import de.unibonn.iai.eis.diachron.configuration.DataSetMappingForTestCase;
-import de.unibonn.iai.eis.diachron.qualitymetrics.intrinsic.consistency.OntologyHijackingTest;
 import de.unibonn.iai.eis.diachron.qualitymetrics.utilities.TestLoader;
 
 /**
@@ -37,10 +40,41 @@ public class ReputationOfDatasetTest {
         
         /**
          * Test method for {@link de.unibonn.iai.eis.diachron.qualitymetrics.reputation.ReputationOfDataset#compute(com.hp.hpl.jena.sparql.core.Quad)}.
+         * 
+         * Total Resources [Count = 27]
+         * Total Not Reputable Resources [Count = 20]
+         * metric value = 20 / 27 = 0.74074074
+         * 
+         * List of Not Reputable Resources
+         * ------------------------------------
+         * http://helloWorld.org/#spiderman
+         * http://example.org/entity/0.1/name
+         * http://helloWorld.org/#spiderman
+         * http://example.org/entity/0.1/name
+         * http://helloWorld.org/#spiderman
+         * http://example.org/entity/0.1/Person
+         * http://helloWorld.org/#spiderman
+         * http://www.perceive.net/schemas/relationship/enemyOf
+         * http://helloWorld.org/#green-goblin
+         * http://helloWorld.org/#John
+         * http://helloWorld.org/#Person
+         * http://helloWorld.org/#green-goblin
+         * http://example.org/entity/0.1/name
+         * http://helloWorld.org/#green-goblin
+         * http://example.org/entity/0.1/Person
+         * http://helloWorld.org/#green-goblin
+         * http://www.perceive.net/schemas/relationship/enemyOf
+         * http://helloWorld.org/#spiderman
+         * http://helloWorld.org/#Person
+         * http://helloWorld.org/#married
          */
         @Test
         public final void testCompute() {
-                fail("Not yet implemented"); // TODO
+                List<Quad> streamingQuads = loader.getStreamingQuads();
+                for(Quad quad : streamingQuads){
+                        reputationOfDataset.compute(quad);
+                }
+                assertEquals(0.74074074, reputationOfDataset.metricValue(), 0.00001);
         }
 
 }
