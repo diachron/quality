3f222ad95dd0658360bf4b53f70faac7cf0364ae
jerdeb <jerdebattista@gmail.com>
2014-08-14 12:30:31 +0200

    updated script

--- assessment.sh
+++ assessment.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 if [ "$1" == build ] ; then
+	echo "Compiling Diachron Quality Metrics";
+	mvn clean install -Dmaven.test.skip=true;
+	echo "Copying Quality Metrics JAR to Luzzu Quality Framework";
+	mkdir Luzzu/luzzu-communications/externals/diachron/;
+	cp target/diachron-0.0.1-SNAPSHOT.jar Luzzu/luzzu-communications/externals/diachron/;
+	cp metrics.trig Luzzu/luzzu-communications/externals/diachron/;
+	echo "Copying DQM vocabulary to Luzzu Quality Framework";
+	cp src/main/resources/vocabularies/dqm/dqm.trig Luzzu/luzzu-communications/externalvocab/;
+	echo "Compiling Luzzu Quality Framework";
 	cd Luzzu/;
 	mvn clean compile;
+	echo "Build Finished"
 elif [ "$1" == run ] ; then
 	cd Luzzu/luzzu-communications/;
 	mvn exec:java -X;
+else
+	echo "Please first run 'assessment.sh build' then 'assessment.sh run'";
 fi
