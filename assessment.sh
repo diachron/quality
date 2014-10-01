968df1786d11830ee353fd39d94141413fbc8250
jerdeb <jerdebattista@gmail.com>
2014-10-01 17:07:43 +0200

    modified assessment.sh - Removed sudo. Do not run the script as sudo

--- assessment.sh
+++ assessment.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 if [ "$1" == build ] ; then
 	echo "Compiling Diachron Quality Metrics";
-	sudo mvn clean install -Dmaven.test.skip=true;
+	mvn clean install -Dmaven.test.skip=true;
 	echo "Copying Quality Metrics JAR to Luzzu Quality Framework";
-	sudo mkdir Luzzu/luzzu-communications/externals/diachron/;
-	sudo cp target/diachron-0.0.1-SNAPSHOT.jar Luzzu/luzzu-communications/externals/diachron/;
-	sudo cp metrics.trig Luzzu/luzzu-communications/externals/diachron/;
+	mkdir Luzzu/luzzu-communications/externals/diachron/;
+	cp target/diachron-0.0.1-SNAPSHOT.jar Luzzu/luzzu-communications/externals/diachron/;
+	cp metrics.trig Luzzu/luzzu-communications/externals/diachron/;
 	echo "Copying DQM vocabulary to Luzzu Quality Framework";
-	sudo cp src/main/resources/vocabularies/dqm/dqm.trig Luzzu/luzzu-communications/externalvocab/;
+	cp src/main/resources/vocabularies/dqm/dqm.trig Luzzu/luzzu-communications/externalvocab/;
 	echo "Compiling Luzzu Quality Framework";
 	cd Luzzu/;
-	sudo mvn clean install;
+	mvn clean install -Dmaven.test.skip=true;
 	echo "Build Finished"
 elif [ "$1" == run ] ; then
 	cd Luzzu/luzzu-communications/;
-	sudo mvn exec:java -X;
+	mvn exec:java -X;
 else
 	echo "Please first run 'assessment.sh build' then 'assessment.sh run'";
 fi
