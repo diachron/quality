55a58c0525c276f76086e8f027c0796fcc5eb1cd
Ruslan Kovalov <ruslan.kovalyov@gmail.com>
2014-10-23 00:58:29 +0200

    Edit the malformed data type metric.

--- src/main/java/eu/diachron/qualitymetrics/intrinsic/accuracy/MalformedDatatypeLiterals.java
+++ src/main/java/eu/diachron/qualitymetrics/intrinsic/accuracy/MalformedDatatypeLiterals.java
@@ -1,6 +1,5 @@
 package eu.diachron.qualitymetrics.intrinsic.accuracy;
 
-import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,8 +7,6 @@ import org.apache.log4j.Logger;
 
 import com.hp.hpl.jena.datatypes.RDFDatatype;
 import com.hp.hpl.jena.graph.Node;
-import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.sparql.core.Quad;
 
@@ -17,120 +14,70 @@ import de.unibonn.iai.eis.luzzu.assessment.QualityMetric;
 import de.unibonn.iai.eis.luzzu.datatypes.ProblemList;
 import de.unibonn.iai.eis.luzzu.exceptions.ProblemListInitialisationException;
 import eu.diachron.semantics.vocabulary.DQM;
+
+
 /**
  * Detects whether the value of a typed literal is valid with respect to its
  * given xsd datatype.
  * 
- * Metric Value Range : [0 - 1]
- * Best Case : 0
- * Worst Case : 1
- * 
- * @author Muhammad Ali Qasmi
- * @date 13th Feb 2014
+ * Metric Value Range : [0 - 1] Best Case : 0 Worst Case : 1
  */
 public class MalformedDatatypeLiterals implements QualityMetric {
-	/**
-	 * Metric URI
-	 */
-	private final Resource METRIC_URI = DQM.MalformedDatatypeLiteralsMetric;
-	/**
-	 * logger static object
-	 */
-	private static Logger logger = Logger
-			.getLogger(MalformedDatatypeLiterals.class);
-	/**
-	 * total number of literals
-	 */
-	private double totalLiterals = 0;
-	/**
-	 * total number of malformed literals
-	 */
-	private double malformedLiterals = 0;
-	/**
-	 * list of problematic quads
-	 */
-	protected List<Quad> problemList = new ArrayList<Quad>();
+	private static final Logger LOG = Logger.getLogger(MalformedDatatypeLiterals.class);
+	private static final Resource METRIC_URI = DQM.MalformedDatatypeLiteralsMetric;
+
+	private List<Quad> problems = new ArrayList<Quad>();
+
+	private int literals = 0;
+	private int malformedLiterals = 0;
 
 	/**
-	 * This method identify whether a given quad is malformed or not.
-	 * 
-	 * @param quad
-	 *            - to be identified
+	 * Identifies whether a given quad is malformed.
+	 * @param quad A quad to check for quality problems.
 	 */
-	
 	public void compute(Quad quad) {
-		logger.trace("compute() --Started--");
-		// retrieves object from statement
 		Node object = quad.getObject();
-		// checks if object is a literal
 		if (object.isLiteral()) {
-			// retrieves rdfDataType from literal
-			RDFDatatype rdfdataType = object.getLiteralDatatype();
-			// check if rdf data type is a valid data type
-			if (rdfdataType != null) {
-				logger.debug("RdfDataTypeLiteral :: " + object.toString());
-				if (!rdfdataType.isValidLiteral(object.getLiteral())) {
-					this.malformedLiterals++;
-					this.problemList.add(quad);
-					logger.debug("MalformedRDFDataTypeLiteral :: "
-							+ object.toString());
-				}
-				this.totalLiterals++;
-			}
-			logger.debug("Literal :: " + object.toString());
+			detectMalformedDatatype(quad);
+			literals++;
 		}
-		logger.debug("Object :: " + object.toString());
-		logger.trace("compute() --Ended--");
+	}
+
+	private void detectMalformedDatatype(Quad quad) {
+		Node object = quad.getObject();
+		RDFDatatype rdfdataType = object.getLiteralDatatype();
+
+		if (rdfdataType != null && !rdfdataType.isValidLiteral(object.getLiteral())) {
+			malformedLiterals++;
+			problems.add(quad);
+			LOG.info(String.format("Malformed literal is found in quad: %s", quad.toString()));
+		}
 	}
 
 	/**
-	 * Returns metric value for object this class
-	 * 
-	 * @return (number of malformed literals) / (total number of literals)
+	 * Calculates a metric value. Ratio of literals with malformed data types to
+	 * total number of literals.
+	 * @return Ratio of literals with malformed data types to total number of literals.
 	 */
-	
 	public double metricValue() {
-
-		logger.trace("metricValue() --Started--");
-		logger.debug("Malformed Literals :: " + this.malformedLiterals);
-		logger.debug("Total Literals :: " + this.totalLiterals);
-
-		// return ZERO if total number of RDF literals are ZERO [WARN]
-		if (0 >= this.totalLiterals) {
-			logger.warn("Total number of RDF data type literals in given document is found to be zero.");
+		if (literals == 0) {
+			LOG.warn("Total number of literals in given document is 0.");
 			return 0.0;
 		}
-
-		double metricValue = this.malformedLiterals / this.totalLiterals;
-		logger.debug("Metric Value :: " + metricValue);
-		logger.trace("metricValue() --Ended--");
-		return metricValue;
+		return (double) malformedLiterals / (double) literals;
 	}
 
-	/**
-	 * Returns the metric URI
-	 * 
-	 * @return the metric URI
-	 */
-	
 	public Resource getMetricURI() {
-		return this.METRIC_URI;
+		return METRIC_URI;
 	}
 
-	/**
-	 * Returns list of problematic Quads
-	 * 
-	 * @return list of problematic quads
-	 */
-	
 	public ProblemList<?> getQualityProblems() {
-		ProblemList<Quad> tmpProblemList = null;
 		try {
-			tmpProblemList = new ProblemList<Quad>(this.problemList);
-		} catch (ProblemListInitialisationException problemListInitialisationException) {
-			logger.debug(problemListInitialisationException);
-			logger.error(problemListInitialisationException.getMessage());
+			return new ProblemList<Quad>(problems);
+		} catch (ProblemListInitialisationException e) {
+			LOG.error(e.getLocalizedMessage());
 		}
-		return tmpProblemList;
+		// TODO change ProblemList
+		return null;
 	}
 }
--- src/test/java/de/unibonn/iai/eis/diachron/qualitymetrics/intrinsic/accuracy/MalformedDatatypeLiteralsTest.java
+++ src/test/java/de/unibonn/iai/eis/diachron/qualitymetrics/intrinsic/accuracy/MalformedDatatypeLiteralsTest.java
@@ -1,91 +1,68 @@
-/**
- * 
- */
 package de.unibonn.iai.eis.diachron.qualitymetrics.intrinsic.accuracy;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.Logger;
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.hp.hpl.jena.datatypes.xsd.XSDDatatype;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.ResourceFactory;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
 import com.hp.hpl.jena.sparql.core.Quad;
+import com.hp.hpl.jena.sparql.vocabulary.FOAF;
 
-import de.unibonn.iai.eis.diachron.configuration.DataSetMappingForTestCase;
-import de.unibonn.iai.eis.diachron.configuration.OutputFileMappingForQualityProblems;
-import de.unibonn.iai.eis.diachron.qualitymetrics.utilities.TestLoader;
+import de.unibonn.iai.eis.luzzu.assessment.QualityMetric;
+import de.unibonn.iai.eis.luzzu.datatypes.ProblemList;
 import eu.diachron.qualitymetrics.intrinsic.accuracy.MalformedDatatypeLiterals;
 
-/**
- * Test class for {@link eu.diachron.qualitymetrics.intrinsic.accuracy.MalformedDatatypeLiterals#compute(com.hp.hpl.jena.sparql.core.Quad)}. 
- * 
- * @author Muhammad Ali Qasmi
- * @date 13th Feb 2014
- */
-public class MalformedDatatypeLiteralsTest extends Assert {
+public class MalformedDatatypeLiteralsTest {
 
-	static Logger logger = Logger.getLogger(MalformedDatatypeLiteralsTest.class);
-	
-	protected TestLoader loader = new TestLoader();
-	protected MalformedDatatypeLiterals malformedDatatypeLiterals = new MalformedDatatypeLiterals();
+	private static QualityMetric metric;
+	private static List<Quad> quads = new ArrayList<Quad>();
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
-		BasicConfigurator.configure();
-		loader.loadDataSet(DataSetMappingForTestCase.MalformedDatatypeLiterals);
-	}
+	@BeforeClass
+	public static void setUp() throws Exception {
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@After
-	public void tearDown() throws Exception {
-	}
+		Model model = ModelFactory.createDefaultModel();
+		model.createResource("http://example.org/#spiderman")
+				.addLiteral(FOAF.birthday, ResourceFactory.createTypedLiteral("2012-03-11", XSDDatatype.XSDdate))
+				.addLiteral(FOAF.givenname, ResourceFactory.createTypedLiteral("2012-03-11", XSDDatatype.XSDdate))
+				.addLiteral(FOAF.name, ResourceFactory.createTypedLiteral("name", XSDDatatype.XSDstring))
+				.addLiteral(FOAF.aimChatID, ResourceFactory.createTypedLiteral("3333", XSDDatatype.XSDboolean));
 
-	/**
-	 * Test method for {@link eu.diachron.qualitymetrics.intrinsic.accuracy.MalformedDatatypeLiterals#compute(com.hp.hpl.jena.sparql.core.Quad)}.
-	 */
-	@Test
-	public final void testCompute() {
-	    List<Quad> streamingQuads = loader.getStreamingQuads();
-		for(Quad quad : streamingQuads){
-			malformedDatatypeLiterals.compute(quad);
+		StmtIterator si = model.listStatements();
+		while (si.hasNext()) {
+			quads.add(new Quad(null, si.next().asTriple()));
 		}
-		assertEquals(0.166666,malformedDatatypeLiterals.metricValue(), 0.00001);
 	}
-	
-	/**
-	 * Test method for {@link de.unibonn.iai.eis.diachron.qualitymetrics.intrinsic.accuracy.MalformedDatatypeLiterals#compute(com.hp.hpl.jena.sparql.core.Quad)}.
-	 */
-	/*
+
 	@Test
-	public final void testOutProblematicInstancesToStream() {
-        try {
-                
-            List<Quad> streamingQuads = loader.getStreamingQuads();
-            for(Quad quad : streamingQuads){
-                malformedDatatypeLiterals.compute(quad);
-            }
-                
-            OutputStream tmpStream = null;
-            tmpStream = new FileOutputStream(OutputFileMappingForQualityProblems.MalformedDatatypeLiterals);
-            malformedDatatypeLiterals.outProblematicInstancesToStream(DataSetMappingForTestCase.MalformedDatatypeLiterals,tmpStream);
-            tmpStream.close();
-        } catch (FileNotFoundException e) {
-                e.printStackTrace();
-        } catch (IOException e) {
-                e.printStackTrace();
-        }
+	public void test() throws InstantiationException, IllegalAccessException {
+		metric = new MalformedDatatypeLiterals();
+
+		for (Quad quad : quads) {
+			metric.compute(quad);
+		}
+		metric.metricValue();
+		ProblemList<?> problems = metric.getQualityProblems();
+		Assert.assertFalse(problems.getProblemList().isEmpty());
+		Assert.assertTrue(problems.getProblemList().size() == 1);
+		Assert.assertEquals(0.25, metric.metricValue(), 0.0);
 	}
-	*/
+
+	// @Test
+	public void emptyQuads() {
+		metric = new MalformedDatatypeLiterals();
+
+		// metric.compute(new ArrayList<Quad>());
+		ProblemList<?> problems = metric.getQualityProblems();
+		Assert.assertTrue(problems.getProblemList().isEmpty());
+		Assert.assertTrue(problems.getProblemList().size() == 0);
+		Assert.assertEquals(0.0, metric.metricValue(), 0.0);
+	}
+
 }
