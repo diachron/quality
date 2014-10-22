b240a80c29feb7c8ec5fee1ad4e8e9571665d45f
Ruslan Kovalov <ruslan.kovalyov@gmail.com>
2014-10-23 00:56:57 +0200

    Add resource files. Edit empty annotation metric.

--- src/main/java/eu/diachron/qualitymetrics/representational/understandability/EmptyAnnotationValue.java
+++ src/main/java/eu/diachron/qualitymetrics/representational/understandability/EmptyAnnotationValue.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -15,14 +14,13 @@ import org.apache.log4j.Logger;
 import org.apache.xerces.util.URI;
 
 import com.hp.hpl.jena.graph.Node;
-import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.sparql.core.Quad;
 
-import de.unibonn.iai.eis.luzzu.assessment.QualityMetric;
+import de.unibonn.iai.eis.luzzu.assessment.ComplexQualityMetric;
 import de.unibonn.iai.eis.luzzu.datatypes.ProblemList;
 import de.unibonn.iai.eis.luzzu.exceptions.ProblemListInitialisationException;
+import eu.diachron.qualitymetrics.utilities.Constants;
 import eu.diachron.semantics.vocabulary.DQM;
 
 /**
@@ -30,200 +28,114 @@ import eu.diachron.semantics.vocabulary.DQM;
  * (labels, comments, notes, etc.) and identifies triples whose property is from
  * a pre-configured list of annotation properties, and whose object is an empty
  * string.
- * 
+ *
  * list of widely used annotation properties are stored in
  * ..src/main/resources/AnnotationPropertiesList.txt
- * 
- * The metric value is defined as the ratio of annotations with empty objects to 
+ *
+ * The metric value is defined as the ratio of annotations with empty objects to
  * all annotations (triples having such properties).
- * 
+ *
  * Metric value Range = [0 - 1] Best Case = 0 Worst Case = 1
- * 
+ *
  * This metric is from the list of constrains for scientific pilots and is
  * introduced in the Deliverable 3.1 (Table 20)
- * 
- * @author Muhammad Ali Qasmi
- * @date 19th June 2014
  */
-public class EmptyAnnotationValue implements QualityMetric {
+public class EmptyAnnotationValue implements ComplexQualityMetric {
+	private static final Logger LOG = Logger.getLogger(EmptyAnnotationValue.class);
+
+	private static final Resource METRIC_URI = DQM.EmptyAnnotationValueMetric;
+
+	private long literals = 0;
+	private long emptyLiterals = 0;
+
+	private static Set<String> properties = new HashSet<String>();
+	private List<Quad> problems = new ArrayList<Quad>();
 
 	/**
-	 * Metric URI
+	 * Loads a annotation properties.
+	 * @param args Arguments, args[0] is a path to annotation properties file.
 	 */
-	private final Resource METRIC_URI = DQM.EmptyAnnotationValueMetric;
-	/**
-	 * logger static object
-	 */
-	static Logger logger = Logger.getLogger(EmptyAnnotationValue.class);
-	/**
-	 * list of problematic quads
-	 */
-	protected List<Quad> problemList = new ArrayList<Quad>();
-	/**
-	 * Default file path and name for the file that contains list of annotation
-	 * properties
-	 */
-	protected static String defaultFilePathName = "src/main/resources/AnnotationPropertiesList.txt";
-	/**
-	 * Number of literals count
-	 */
-	protected long totalNumberOfLiterals = 0;
-	/**
-	 * Number of empty literals count
-	 */
-	protected long totalNumberOfEmptyLiterals = 0;
-	/**
-	 * list of annotation properties to be evaluated.
-	 */
-	protected static Set<String> annotationPropertiesSet = new HashSet<String>();
-
-	/**
-	 * loads list of annotation properties in set
-	 * 
-	 * if filePathName is null then default path will be used.
-	 * 
-	 * @param filePathName
-	 *            - file Path and name, default :
-	 *            src/main/resources/AnnotationPropertiesList.txt
-	 */
-	public static void loadAnnotationPropertiesSet(String filePathName) {
+	public void before(Object... args) {
+		String path = (args == null || args.length == 0) ? Constants.ANNOTATION_PROPERTIES_FILE
+				: (String) args[0];
 		File file = null;
-		String tmpFilePathName = (filePathName == null) ? EmptyAnnotationValue.defaultFilePathName
-				: filePathName;
 		try {
-			if (!tmpFilePathName.isEmpty()) {
-				file = new File(tmpFilePathName);
-				if (file.exists() && file.isFile()) {
-					String strLine = null;
-					BufferedReader in = new BufferedReader(new FileReader(file));
-					while ((strLine = in.readLine()) != null) {
-						URI tmpURI = new URI(strLine);
-						if (tmpURI != null) {
-							EmptyAnnotationValue.annotationPropertiesSet
-									.add(strLine);
-						}
+			file = new File(path);
+			if (file.exists() && file.isFile()) {
+				String line = null;
+				BufferedReader in = new BufferedReader(new FileReader(file));
+				while ((line = in.readLine()) != null && !line.isEmpty()) {
+					if (new URI(line.trim()) != null) {
+						properties.add(line);
 					}
-					in.close();
 				}
+				in.close();
 			}
 		} catch (FileNotFoundException e) {
-			logger.debug(e.getStackTrace());
-			logger.error(e.getMessage());
+			LOG.error(e.getLocalizedMessage());
 		} catch (IOException e) {
-			logger.debug(e.getStackTrace());
-			logger.error(e.getMessage());
+			LOG.error(e.getLocalizedMessage());
 		}
 	}
 
 	/**
-	 * clears list of annotation properties in set
+	 * The method checks if an object's value is empty.
+	 * @param quad A quad to check for quality problems.
 	 */
-	public static void clearAnnotationPropertiesSet() {
-		EmptyAnnotationValue.annotationPropertiesSet.clear();
-	}
-
-	/**
-	 * Checks whether given quad has predicate with URI found in annotation
-	 * properties set if true then checks the object's value in that quad;
-	 * whether it is empty or not.
-	 * 
-	 */
-	
 	public void compute(Quad quad) {
-		try {
-			Node predicate = quad.getPredicate();
-			if (predicate.isURI()) { // check is the predicate is URI or not
-				if (EmptyAnnotationValue.annotationPropertiesSet
-						.contains(predicate.getURI())) { // check if given
-															// predicate is
-															// found in
-															// annotation
-															// properties list
-					boolean isEmptyLiteral = false; // set empty literal to
-													// false
-					Node object = quad.getObject();
-					this.totalNumberOfLiterals++; // increment total number of
-													// literals
-					if (object.isBlank()) { // check blank object
-						isEmptyLiteral = true; // set empty literal to true
-					} else if (object.isLiteral()) { // check whether object is
-														// literal or not
-						String value = object.getLiteralValue().toString(); // retrieve
-																			// object's
-																			// value
-						value = value.trim(); // removes whitespace from both
-												// ends
-						if (value == null) { // check if object's value is null
-												// or not
-							isEmptyLiteral = true; // set empty literal to true
-						} else if (value.isEmpty()) { // check is object's value
-														// is empty
-							isEmptyLiteral = true; // set empty literal to true
-						}
-					} else {
-						isEmptyLiteral = true;
-					}
+		Node predicate = quad.getPredicate();
+		if (predicate.isURI() && properties.contains(predicate.getURI())) {
+			literals++;
 
-					if (isEmptyLiteral) {
-						this.totalNumberOfEmptyLiterals++; // increment empty
-															// literal count
-						this.problemList.add(quad); // add invalid quad in
-													// problem list
-					}
-				}
+			if (isEmptyLiteral(quad)) {
+				emptyLiterals++;
+				problems.add(quad);
+				LOG.info(String.format("Empty annotation is found in quad: %s", quad.toString()));
 			}
-		} catch (Exception e) {
-			logger.debug(e.getStackTrace());
-			logger.debug(e.getMessage());
 		}
 	}
 
+	private boolean isEmptyLiteral(Quad quad) {
+		Node object = quad.getObject();
+		if (object.isBlank()) {
+			return true;
+		} else if (object.isLiteral()) {
+			String value = object.getLiteralValue().toString().trim();
+			if (value.isEmpty()) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	/**
-	 * metric value = total number of empty literals / total number of literals
-	 * 
-	 * @return ( (total number of empty literals) / (total number of literals) )
+	 * Calculates a metric value. Ratio of empty annotation literals to total
+	 * number of literals.
+	 * @return Ratio of empty annotation literals to the total number of literals.
 	 */
-	
 	public double metricValue() {
-		logger.debug("Total number of empty literals : "
-				+ this.totalNumberOfEmptyLiterals);
-		logger.debug("Total total number of literals : "
-				+ this.totalNumberOfLiterals);
-		if (this.totalNumberOfLiterals <= 0) {
-			logger.warn("Total total number of literals are ZERO");
+		if (literals == 0) {
+			LOG.warn("Total number of literals are ZERO");
 			return 0;
 		}
-		return ((double) this.totalNumberOfEmptyLiterals / (double) this.totalNumberOfLiterals);
+		return (double) emptyLiterals / (double) literals;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * de.unibonn.iai.eis.diachron.qualitymetrics.AbstractQualityMetric#getMetricURI
-	 * ()
-	 */
-	
 	public Resource getMetricURI() {
-		return this.METRIC_URI;
+		return METRIC_URI;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see de.unibonn.iai.eis.diachron.qualitymetrics.AbstractQualityMetric#
-	 * getQualityProblems()
-	 */
-	
 	public ProblemList<?> getQualityProblems() {
-		ProblemList<Quad> tmpProblemList = null;
 		try {
-			tmpProblemList = new ProblemList<Quad>(this.problemList);
-		} catch (ProblemListInitialisationException problemListInitialisationException) {
-			logger.debug(problemListInitialisationException.getStackTrace());
-			logger.error(problemListInitialisationException.getMessage());
+			return new ProblemList<Quad>(problems);
+		} catch (ProblemListInitialisationException e) {
+			LOG.error(e.getLocalizedMessage());
 		}
-		return tmpProblemList;
+		// TODO change ProblemList
+		return null;
 	}
-	
+
+	public void after(Object... arg0) {
+		properties.clear();
+	}
 }
