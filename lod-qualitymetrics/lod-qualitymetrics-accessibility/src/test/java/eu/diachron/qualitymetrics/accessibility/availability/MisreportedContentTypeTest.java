a9c719948610b45457dd3e19ff7aebf96fbda824
jerdeb <jerdebattista@gmail.com>
2015-02-12 15:40:38 +0100

    removed unused imports

--- lod-qualitymetrics/lod-qualitymetrics-accessibility/src/test/java/eu/diachron/qualitymetrics/accessibility/availability/MisreportedContentTypeTest.java
+++ lod-qualitymetrics/lod-qualitymetrics-accessibility/src/test/java/eu/diachron/qualitymetrics/accessibility/availability/MisreportedContentTypeTest.java
@@ -1,8 +1,6 @@
 package eu.diachron.qualitymetrics.accessibility.availability;
 
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.junit.After;
 import org.junit.Assert;
