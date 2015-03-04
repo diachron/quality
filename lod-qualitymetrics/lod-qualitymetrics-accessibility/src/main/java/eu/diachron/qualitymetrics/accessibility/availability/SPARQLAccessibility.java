22443211b507b5dadd7600bc760630dfcfaa2238
jerdeb <jerdebattista@gmail.com>
2015-03-04 14:50:37 +0100

    added sd:endpoint as possible property defining a sparql endpoint

--- lod-qualitymetrics/lod-qualitymetrics-accessibility/src/main/java/eu/diachron/qualitymetrics/accessibility/availability/SPARQLAccessibility.java
+++ lod-qualitymetrics/lod-qualitymetrics-accessibility/src/main/java/eu/diachron/qualitymetrics/accessibility/availability/SPARQLAccessibility.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.apache.log4j.Logger;
 
 import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryException;
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
@@ -15,6 +16,7 @@ import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.sparql.core.Quad;
 
 import de.unibonn.iai.eis.diachron.semantics.DQM;
+import de.unibonn.iai.eis.diachron.semantics.knownvocabs.SD;
 import de.unibonn.iai.eis.diachron.technques.probabilistic.ResourceBaseURIOracle;
 import de.unibonn.iai.eis.luzzu.assessment.QualityMetric;
 import de.unibonn.iai.eis.luzzu.datatypes.ProblemList;
@@ -45,27 +47,41 @@ public class SPARQLAccessibility implements QualityMetric {
 	double totalDefinedSparqlEndPoints = 0.0;
 	
 	private ResourceBaseURIOracle oracle = new ResourceBaseURIOracle();
-
+	
+	final static List<Resource> endpointProperty = new ArrayList<Resource>();
+	static{
+		endpointProperty.add(VOID.sparqlEndpoint);
+//		endpointProperty.add(SIOCSERVICES.service_endpoint);
+		endpointProperty.add(SD.endpoint);
+	}
+	
+	
 	
 	public void compute(Quad quad) {
 		oracle.addHint(quad);
 		
-		if (quad.getPredicate().getURI().equals(VOID.sparqlEndpoint.getURI())) {
+		if (endpointProperty.contains(quad.getPredicate())) {
 			String sparqlQuerystring = "SELECT ?s where {?s ?p ?o} LIMIT 1";
 			Query query = QueryFactory.create(sparqlQuerystring);
 
 			QueryExecution qexec = QueryExecutionFactory.sparqlService(quad.getObject().toString(), query);
 
-			ResultSet results = qexec.execSelect();
-
-			if (results.hasNext())
-				sparqlEndPoints++;
-			else
-			{
+			try{
+				ResultSet results = qexec.execSelect();
+	
+				if (results.hasNext())
+					sparqlEndPoints++;
+				else
+				{
+					Quad q = new Quad(null, quad.getSubject() , QPRO.exceptionDescription.asNode(), DQM.InvalidSPARQLEndPoint.asNode());
+					problemList.add(q);
+				}
+				qexec.close();
+			} catch (QueryException e){
+				logger.error("Endpoint " + quad.getObject().toString() + " responded with : " + e.getMessage());
 				Quad q = new Quad(null, quad.getSubject() , QPRO.exceptionDescription.asNode(), DQM.InvalidSPARQLEndPoint.asNode());
 				problemList.add(q);
 			}
-			qexec.close();
 			totalDefinedSparqlEndPoints++;
 		}
 	}
@@ -107,5 +123,4 @@ public class SPARQLAccessibility implements QualityMetric {
 	public Resource getAgentURI() {
 		return 	DQM.LuzzuProvenanceAgent;
 	}
-	
 }
