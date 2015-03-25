2e898b2c40775b0823b3feb4b4da952a8a0228aa
jerdeb <jerdebattista@gmail.com>
2015-03-25 16:11:56 +0100

    added metric for reusing existing vocabularies + test

---
+++ lod-qualitymetrics/lod-qualitymetrics-representation/src/main/java/eu/diachron/qualitymetrics/representational/interoperability/ReuseExistingVocabularies.java
@@ -0,0 +1,224 @@
+/**
+ * 
+ */
+package eu.diachron.qualitymetrics.representational.interoperability;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.riot.RiotException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.hp.hpl.jena.graph.Node;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.sparql.core.Quad;
+import com.hp.hpl.jena.vocabulary.RDF;
+
+import de.unibonn.iai.eis.diachron.mapdb.MapDbFactory;
+import de.unibonn.iai.eis.diachron.semantics.DQM;
+import de.unibonn.iai.eis.luzzu.assessment.ComplexQualityMetric;
+import de.unibonn.iai.eis.luzzu.datatypes.ProblemList;
+import de.unibonn.iai.eis.luzzu.exceptions.AfterException;
+import de.unibonn.iai.eis.luzzu.exceptions.BeforeException;
+import de.unibonn.iai.eis.luzzu.exceptions.ProblemListInitialisationException;
+import de.unibonn.iai.eis.luzzu.properties.EnvironmentProperties;
+import de.unibonn.iai.eis.luzzu.semantics.vocabularies.QPRO;
+import eu.diachron.qualitymetrics.representational.utils.SharedResources;
+import eu.diachron.qualitymetrics.utilities.LOVInterface;
+import eu.diachron.qualitymetrics.utilities.VocabularyLoader;
+
+/**
+ * @author Jeremy Debattista
+ * 
+ * This metric calculates the number of unique vocabularies
+ * used in a dataset, but suggested to be useful in the
+ * dataset's domain. This metric is similar to the
+ * Reuse Existing Terms metric with the difference that
+ * we do not look at overlapping terms. In this metric,
+ * we also check if the term used is defined or not before
+ * adding to the seen-suggested set.
+ * 
+ * The value of this metric is calculated as follows:
+ * 	# of used ns from suggested list / total # of suggested vocabularies  
+ * 
+ * Config File Example:
+ * For example:
+ * <http://linkedgeodata.org> :hasDomain "Geographic" ;
+ *  :hasVocabularies <http://www.w3.org/2003/01/geo/wgs84_pos#> , <http://www.geonames.org/ontology#> ;
+ *  :getFromLOV "True" 
+ */
+public class ReuseExistingVocabularies implements ComplexQualityMetric {
+
+	private Model categories = ModelFactory.createDefaultModel();
+
+	private Set<String> suggestedVocabs = new HashSet<String>();
+	private Set<String> seenSuggested = new HashSet<String>();
+	
+	private SharedResources shared = SharedResources.getInstance();
+	private Set<String> seenSet = MapDbFactory.createFilesystemDB().createHashSet("seen-set").make();
+	
+	private static Logger logger = LoggerFactory.getLogger(ReuseExistingVocabularies.class);
+
+	private List<Quad> _problemList = new ArrayList<Quad>();
+
+
+	@Override
+	public void compute(Quad quad) {
+		Node predicate = quad.getPredicate();
+		
+		if (predicate.hasURI(RDF.type.toString())){
+			//it is a class
+			Node object = quad.getObject();
+			logger.info("checking class: {}", object.getURI());
+
+			if (!(object.isBlank())){
+				if (!(this.seenSet.contains(object.getURI()))){
+					if (suggestedVocabs.contains(object.getNameSpace())){
+						Boolean seen = shared.classOrPropertyDefined(object.getURI());
+						Boolean defined = null;
+						if (seen == null) {
+							defined = VocabularyLoader.checkTerm(object);
+							shared.addClassOrProperty(object.getURI(), defined);
+						}
+						else defined = seen;
+						
+						if (defined){
+							seenSuggested.add(object.getNameSpace());
+						}
+						this.seenSet.add(object.getURI());
+					}
+				}
+			}
+		}
+		
+		if (!(this.seenSet.contains(predicate.getURI()))){
+			if (suggestedVocabs.contains(predicate.getNameSpace())){
+				Boolean seen = shared.classOrPropertyDefined(predicate.getURI());
+				Boolean defined = null;
+				if (seen == null) {
+					defined = VocabularyLoader.checkTerm(predicate);
+					shared.addClassOrProperty(predicate.getURI(), defined);
+				}
+				else defined = seen;
+				
+				if (defined){
+					seenSuggested.add(predicate.getNameSpace());
+				}
+			}
+			this.seenSet.add(predicate.getURI());
+		}
+	}
+
+	@Override
+	public double metricValue() {
+		double reusedvoc = ((double)this.seenSuggested.size()) / ((double) this.suggestedVocabs.size());
+		
+		for(String s : suggestedVocabs){
+			if (!(seenSuggested.contains(s))){
+				Quad q = new Quad(null, ModelFactory.createDefaultModel().createResource(s).asNode(), QPRO.exceptionDescription.asNode(), DQM.UnusedSuggestedVocabulary.asNode());
+				this._problemList.add(q);
+			}
+		}
+		
+		return reusedvoc;
+	}
+
+	@Override
+	public Resource getMetricURI() {
+		return DQM.ReuseExistingTermsMetric;
+	}
+
+	@Override
+	public ProblemList<?> getQualityProblems() {
+		ProblemList<Quad> pl = null;
+		try {
+			pl = new ProblemList<Quad>(this._problemList);
+		} catch (ProblemListInitialisationException e) {
+			logger.error(e.getMessage());
+		}
+		return pl;
+	}
+
+	@Override
+	public boolean isEstimate() {
+		return false;
+	}
+
+	@Override
+	public Resource getAgentURI() {
+		return 	DQM.LuzzuProvenanceAgent;
+	}
+
+	/** 
+	 * The before method in this method should accept only
+	 * a string with the trig filename 
+	 */
+	@Override
+	public void before(Object [] args) throws BeforeException {
+		if (args[0] == null){
+			logger.error("Argument in the Reuse Existing Vocabulary metric should not be null.");
+			throw new BeforeException("Argument should not be a string with a filename.");
+		}
+		String fileName = (String) args[0];
+		
+		try{
+			categories = RDFDataMgr.loadModel(fileName);
+		} catch (RiotException e){
+			logger.error("Error loading configuration file: {}", fileName);
+			throw new BeforeException("Invalid configuration file passed to the Reuse Existing Vocabulary metric. The configuration file should be RDF " + 
+					"(or any other RDF serialisation format), having this following format: <http://linkedgeodata.org> :hasDomain \"geographic\"^^xsd:string ; " + 
+					" :hasVocabularies <http://www.w3.org/2003/01/geo/wgs84_pos#> , <http://www.geonames.org/ontology#> .  ");
+		}
+		
+		//Get dataset/base URI 
+		String baseURI = EnvironmentProperties.getInstance().getBaseURI();
+		if (baseURI == null){
+			//Try using the dataset URI
+			baseURI = EnvironmentProperties.getInstance().getBaseURI();
+			if (baseURI == null){
+				logger.error("Unknown Dataset URI");
+				throw new BeforeException("Dataset URI is not known. This should be set in Luzzu.");
+			}
+		}
+		
+		//Load Vocabularies
+		logger.info("Getting vocabularies from user configuration file for {}",baseURI);
+		List<RDFNode> lst = categories.listObjectsOfProperty(categories.createResource(baseURI), categories.createProperty(":hasVocabularies")).toList();
+		logger.info("Found {} vocabularies", lst.size());
+		for(RDFNode n : lst){
+			logger.info("Loading {}", n.toString());
+			VocabularyLoader.loadVocabulary(n.toString());
+			suggestedVocabs.add(n.toString());
+		}
+		
+		List<RDFNode> lov = categories.listObjectsOfProperty(categories.createResource(baseURI), categories.createProperty(":getFromLOV")).toList();
+		boolean getFromLov = false;
+		if (lov.size() > 0) getFromLov = lov.get(0).asLiteral().getBoolean();
+		
+		if (getFromLov){
+			List<RDFNode> domainNode = categories.listObjectsOfProperty(categories.createResource(baseURI), categories.createProperty(":hasDomain")).toList();
+			for(RDFNode n : domainNode){
+				try {
+					List<String> vocabs = LOVInterface.getKnownVocabsPerDomain(n.asLiteral().getString());
+					for(String v : vocabs) suggestedVocabs.add(v);
+				} catch (IOException e) {
+					logger.error("Could not load vocabularies from LOD. Error Message: {}", e.getMessage());
+				}
+			}
+		}
+	}
+
+	@Override
+	public void after(Object [] args) throws AfterException {
+		// nothing to do
+	}
+	
+}
