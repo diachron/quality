87217b339e0908aa1478e0939a4df8ca58005093
jerdeb <jerdebattista@gmail.com>
2015-05-15 17:42:31 +0200

    fixed examples

--- examples/efo241.sh
+++ examples/efo241.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 var=$(pwd)
 fileName="$var/efo-2.41.rdf"
-curl --data "Dataset=$fileName&QualityReportRequired=false&MetricsConfiguration={\"@id\":\"_:f4216607408b1\",\"@type\":[\"http://github.com/EIS-Bonn/Luzzu#MetricConfiguration\"],\"http://github.com/EIS-Bonn/Luzzu#metric\":[{\"@value\":\"eu.diachron.qualitymetrics.accessibility.availability.SPARQLAccessibility\"}]}" http://localhost:8080/Luzzu/compute_quality ;
+curl --data "BaseUri=http://www.ebi.ac.uk/efo/&Dataset=$fileName&QualityReportRequired=false&MetricsConfiguration={\"@id\":\"_:f4216607408b1\",\"@type\":[\"http://github.com/EIS-Bonn/Luzzu#MetricConfiguration\"],\"http://github.com/EIS-Bonn/Luzzu#metric\":[{\"@value\":\"de.unibonn.iai.eis.diachron.ebi.DefinedOntologyAuthor\"},{\"@value\":\"de.unibonn.iai.eis.diachron.ebi.OBOFoundry\"},{\"@value\":\"de.unibonn.iai.eis.diachron.ebi.ObsoleteConceptsInOntology\"},{\"@value\":\"de.unibonn.iai.eis.diachron.ebi.POBODefinitionUsage\"},{\"@value\":\"de.unibonn.iai.eis.diachron.ebi.SynonymUsage\"},{\"@value\":\"de.unibonn.iai.eis.diachron.ebi.OntologyVersioningConciseness\"}]}" http://localhost:8080/Luzzu/compute_quality ;
