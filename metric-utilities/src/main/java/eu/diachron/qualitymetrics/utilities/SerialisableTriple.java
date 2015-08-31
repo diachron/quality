3a771e13ed21ba3c94f37bbbf05fbacbb3d67e8e
jerdeb <jerdebattista@gmail.com>
2015-08-31 18:02:35 +0200

    fixed problem with generating literals and added equals method

--- metric-utilities/src/main/java/eu/diachron/qualitymetrics/utilities/SerialisableTriple.java
+++ metric-utilities/src/main/java/eu/diachron/qualitymetrics/utilities/SerialisableTriple.java
@@ -9,6 +9,7 @@ import com.hp.hpl.jena.graph.Node;
 import com.hp.hpl.jena.graph.Triple;
 import com.hp.hpl.jena.rdf.model.AnonId;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.rdf.model.Resource;
 
 /**
@@ -26,6 +27,7 @@ public class SerialisableTriple  implements Serializable{
 	private String predicate = "";
 	
 	private boolean isObjectBlank = false;
+	private boolean isObjectLiteral = false;
 	private String object = "";
 	
 	public SerialisableTriple(){}
@@ -41,33 +43,45 @@ public class SerialisableTriple  implements Serializable{
 		
 		Node _obj = this.triple.getObject();
 		if (_obj.isBlank()) isObjectBlank = true;
+		if (_obj.isLiteral()) isObjectLiteral = true;
 		this.object = _obj.toString();
 	}
 	
 	public Triple getTriple(){
-		Resource _sbj, _prd, _obj;
+		Resource _sbj, _prd;
+		RDFNode _obj;
+		
 		if (isSubjectBlank) _sbj = ModelFactory.createDefaultModel().createResource(new AnonId());
 		else _sbj = ModelFactory.createDefaultModel().createResource(subject);
 			
 		_prd = ModelFactory.createDefaultModel().createProperty(predicate);
 		
 		if (isObjectBlank) _obj = ModelFactory.createDefaultModel().createResource(new AnonId());
+		else if (isObjectLiteral) _obj = ModelFactory.createDefaultModel().createLiteral(object);
 		else _obj = ModelFactory.createDefaultModel().createResource(object);
 		
 		return new Triple(_sbj.asNode(), _prd.asNode(), _obj.asNode());
 	}
 	
-//	@Override
-//	public boolean equals(Object other){
-//		if (!(other instanceof SerialisableTriple)) return false;
-//		
-//		SerialisableTriple _otherSerialisableTriple = (SerialisableTriple) other;
-//		Triple _otherTriple = _otherSerialisableTriple.getTriple();
-//		
-//		_otherTriple.equals(this.getTriple());
-//		
-//		return false;
-//	}
+	@Override
+	public boolean equals(Object other){
+		if (!(other instanceof SerialisableTriple)) return false;
+		
+		SerialisableTriple _otherSerialisableTriple = (SerialisableTriple) other;
+		Triple _otherTriple = _otherSerialisableTriple.getTriple();
+		
+		return _otherTriple.equals(this.getTriple());
+	}
+	
+	@Override
+	public int hashCode(){
+		StringBuilder sb = new StringBuilder();
+		sb.append(this.subject);
+		sb.append(this.predicate);
+		sb.append(this.object);
+		
+		return sb.toString().hashCode();
+	}
 }
 	
 	
