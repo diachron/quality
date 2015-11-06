77de682b42dc4eff0b7a185d809f3e85a6de6f43
Jeremy Debattista <jerdebattista@gmail.com>
2015-11-06 16:44:15 +0100

    Update assessment.sh

--- assessment.sh
+++ assessment.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ "$1" == dependencies ] ; then
 	echo "downloading dependencies";
-	wget https://github.com/EIS-Bonn/Luzzu/archive/1.2.tar.gz -O ../Luzzu.tar.gz
+	wget https://github.com/EIS-Bonn/Luzzu/archive/1.2.1.tar.gz -O ../Luzzu.tar.gz
 	tar -xvf ../Luzzu.tar.gz
-	mv Luzzu-1.2/ ../Luzzu/
+	mv Luzzu-1.2.1/ ../Luzzu/
 elif [ "$1" == build ] ; then
 	echo "Compiling Diachron Quality Metrics";
 	mvn clean install -Dmaven.test.skip=true;
