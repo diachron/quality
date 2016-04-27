e76c825d6c17fb2ea63cc6d081b72ba899d83a92
jerdeb <jerdebattista@gmail.com>
2016-04-27 17:34:13 +0200

    added a metric that counts the number of different serialisation formats (similar to number of languages in a dataset)

--- lod-qualitymetrics/lod-qualitymetrics-representation/src/main/java/eu/diachron/qualitymetrics/representational/versatility/DifferentSerialisationFormatsCount.java
+++ lod-qualitymetrics/lod-qualitymetrics-representation/src/main/java/eu/diachron/qualitymetrics/representational/versatility/DifferentSerialisationFormatsCount.java
@@ -4,24 +4,18 @@
 package eu.diachron.qualitymetrics.representational.versatility;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
-
-import org.mapdb.DB;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.hp.hpl.jena.graph.Node;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.sparql.core.Quad;
 
-import de.unibonn.iai.eis.diachron.mapdb.MapDbFactory;
 import de.unibonn.iai.eis.diachron.semantics.DQM;
 import de.unibonn.iai.eis.diachron.semantics.DQMPROB;
-import de.unibonn.iai.eis.diachron.technques.probabilistic.ResourceBaseURIOracle;
 import de.unibonn.iai.eis.luzzu.assessment.QualityMetric;
 import de.unibonn.iai.eis.luzzu.datatypes.ProblemList;
 import de.unibonn.iai.eis.luzzu.exceptions.ProblemListInitialisationException;
@@ -38,28 +32,22 @@ import eu.diachron.qualitymetrics.utilities.SerialisableQuad;
  * the possible formats in the dataset's metadata using the void:feature 
  * predicate.
  * 
- * In this metric we check if in a dataset has 1 or more triples descibing
+ * In this metric we check if in a dataset has triples descibing
  * the different serialisation formats that a dataset is available in, 
  * using the void:feature. A list of possible serialisation formats 
  * can be found: http://www.w3.org/ns/formats/ 
  * 
- * The metric returns 1 if the data published is represented in 2 or more
- * formats.
+ * The metric returns the number of different serialisation formats
+ * in a dataset.
  */
-public class DifferentSerialisationFormats implements QualityMetric{
+public class DifferentSerialisationFormatsCount implements QualityMetric{
 	
-	private static DB mapDb = MapDbFactory.getMapDBAsyncTempFile();
-	private Set<SerialisableQuad> _problemList = MapDbFactory.createHashSet(mapDb, UUID.randomUUID().toString());
+	private Set<Quad> _problemList = new HashSet<Quad>();
 	
-	private static Logger logger = LoggerFactory.getLogger(DifferentSerialisationFormats.class);
+	private static Logger logger = LoggerFactory.getLogger(DifferentSerialisationFormatsCount.class);
 	
-	private String datasetURI = null;
 	
-	ResourceBaseURIOracle oracle = new ResourceBaseURIOracle();
-	
-	private ConcurrentHashMap<String, List<String>> datasetFeatures = new ConcurrentHashMap<String, List<String>>();
-	
-	boolean flag = false;
+	private int featureCount = 0;
 	
 	private static List<String> formats = new ArrayList<String>();
 	static{
@@ -97,13 +85,9 @@ public class DifferentSerialisationFormats implements QualityMetric{
 		Node object = quad.getObject();
 		
 		if (predicate.hasURI(VOID.feature.getURI())){
-			datasetURI = quad.getSubject().getURI();
-			
 			if (object.isURI()){
 				if (formats.contains(object.getURI())) {
-					List<String> features = getOrCreate(datasetURI);
-					features.add(object.getURI());
-					this.datasetFeatures.put(datasetURI, features);
+					featureCount++;
 				}
 				else {
 					Quad q = new Quad(null, object, QPRO.exceptionDescription.asNode(), DQMPROB.IncorrectFormatDefined.asNode());
@@ -113,44 +97,16 @@ public class DifferentSerialisationFormats implements QualityMetric{
 		}
 	}
 
-	private List<String> getOrCreate(String datasetURI){
-		List<String> lst = new ArrayList<String>();
-		if (this.datasetFeatures.containsKey(datasetURI)) lst = this.datasetFeatures.get(datasetURI);
-		else this.datasetFeatures.put(datasetURI, lst);
-		return lst;
-	}
 	
 	
 	@Override
 	public double metricValue() {
-		if (!flag){
-			this.setQualityProblems();
-			flag = true;
-		}
-		
-		int totalNumberDatasets = this.datasetFeatures.size();
-		int datasetsWithMoreThanOneFeature = 0;
-		for(String dataset : this.datasetFeatures.keySet()){
-			List<String> features = this.datasetFeatures.get(dataset);
-			if (features.size() > 1){
-				datasetsWithMoreThanOneFeature++;
-			}
-		}
-		statsLogger.info("Different Serialisation Formats. Dataset: {} - Datasets With More than One Feature {}, Total Number Dataset {}", 
-				EnvironmentProperties.getInstance().getDatasetURI(), datasetsWithMoreThanOneFeature,totalNumberDatasets );
+		statsLogger.info("Different Serialisation Formats. Dataset: {} - Feature Count {}", 
+				EnvironmentProperties.getInstance().getDatasetURI(), featureCount );
 
-		return (double)datasetsWithMoreThanOneFeature / (double) totalNumberDatasets;
+		return (double)featureCount;
 	}
 	
-	private void setQualityProblems(){
-		for(String dataset : this.datasetFeatures.keySet()){
-			List<String> features = this.datasetFeatures.get(dataset);
-			if (features.size() <= 1){
-				Quad q = new Quad(null, ModelFactory.createDefaultModel().createResource(dataset).asNode(), QPRO.exceptionDescription.asNode(), DQMPROB.NoMultipleFormatDefined.asNode());
-				this._problemList.add(new SerialisableQuad(q));
-			}
-		}
-	}
 
 	@Override
 	public Resource getMetricURI() {
@@ -159,12 +115,12 @@ public class DifferentSerialisationFormats implements QualityMetric{
 
 	@Override
 	public ProblemList<?> getQualityProblems() {
-		ProblemList<SerialisableQuad> pl = null;
+		ProblemList<Quad> pl = null;
 		try {
 			if(this._problemList != null && this._problemList.size() > 0) {
-				pl = new ProblemList<SerialisableQuad>(this._problemList);
+				pl = new ProblemList<Quad>(this._problemList);
 			} else {
-				pl = new ProblemList<SerialisableQuad>();
+				pl = new ProblemList<Quad>();
 			}
 		} catch (ProblemListInitialisationException e) {
 			logger.error(e.getMessage());
